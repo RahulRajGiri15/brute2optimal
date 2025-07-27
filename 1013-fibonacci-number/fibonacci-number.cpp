7b98935293269075c8d64550a08f20b64349cad3
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 13:42:42 +0530

    Time: 3 ms (66.44%) | Memory: 8.1 MB (17.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,34 @@
 /////////using recursion 
 
+// class Solution {
+// public:
+//     int fib(int n) {
+//         ////base case
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+
+//         ///////recursive code
+//         return fib(n-1)+fib(n-2);
+//     }
+// };
+/////t-
+/////s-o(1)---recusive stack - o(n)
+
+/////////using momoization
+
 class Solution {
 public:
-    int fib(int n) {
-        ////base case
-        if(n==0) return 0;
-        if(n==1) return 1;
+    int solve(int n,vector<int>&dp){
 
-        ///////recursive code
-        return fib(n-1)+fib(n-2);
+        if(n<=1) return n;
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+
+    }
+    int fib(int n) {
+
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
