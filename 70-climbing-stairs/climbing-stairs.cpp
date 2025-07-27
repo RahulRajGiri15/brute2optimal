6837241d4395cf91c1b30881a2609e9dff1d8712
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 15:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,3 +1,4 @@
+////////recursive solution 
 
 // class Solution {
 // public:
@@ -13,24 +14,50 @@
 //     }
 // };
 
-//////////////
+//////////////dp----memoization 
+
+// class Solution {
+// public:
+//     int solve(int n , vector<int>&dp){
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
+//         ////
+//         if(dp[n] != -1) return dp[n];
+//         int x = solve(n-1,dp);
+//         int y = solve(n-2,dp);
+
+//         return dp[n] = x+y;
+//     }
+//     int climbStairs(int n) {
+        
+//         vector<int> dp(n+1,-1);
+
+//         return solve(n , dp);
+
+//     }
+// };
+
+////////////
 
 class Solution {
 public:
     int solve(int n , vector<int>&dp){
         if(n < 0) return 0;
-        if(n == 0) return 1;
-        ////
-        if(dp[n] != -1) return dp[n];
-        int x = solve(n-1,dp);
-        int y = solve(n-2,dp);
-
-        return dp[n] = x+y;
+        ///////
+        if(n <=2) return n;
+        dp[0] =0;
+        dp[1] =1;
+        dp[2] =2; 
+        /////---- can go with 2 ways {1,1} , {2}
+       
+        //////
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
     int climbStairs(int n) {
-        
-        vector<int> dp(n+1,-1);
-
+        vector<int> dp(n+1);
         return solve(n , dp);
 
     }
