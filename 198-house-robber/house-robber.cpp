cb272e0f1e3adf54695ee1e9074a72957065ad21
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 16:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.52%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,39 @@
+///////////recursion solution
+
+// class Solution {
+// public:
+//     int solve(vector<int>&nums, int i,vector<int>&dp){
+//             if(i >= nums.size()) return 0;
+
+//             //////here thief has option to pick and not pick
+//             int pick = nums[i] + solve(nums,i+2);
+//             int not_pick = solve(nums,i+1);
+            
+//             return max(pick , not_pick);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         return solve(nums,0,dp);
+//     }
+// };
+
+////////////
+
+class Solution {
+public:
+    int solve(vector<int>&nums, int i,vector<int>&dp){
+            if(i >= nums.size()) return 0;
+            
+            if(dp[i] != -1) return dp[i];
+            int pick = nums[i] + solve(nums,i+2,dp);
+            int not_pick = solve(nums,i+1,dp);
+            
+            return dp[i]= max(pick , not_pick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int>dp(n+1,-1);
+        return solve(nums,0,dp);
+    }
+};
