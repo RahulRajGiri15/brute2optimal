c31320009661dbef792bc2744884a7c888225d7d
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 15:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (52.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -13,29 +13,34 @@
 
 //     }
 // };
+//////t-o(2^n)
+/////s-o(1) --- recursive stack - o(n)
 
 //////////////dp----memoization 
 
-// class Solution {
-// public:
-//     int solve(int n , vector<int>&dp){
-//         if(n < 0) return 0;
-//         if(n == 0) return 1;
-//         ////
-//         if(dp[n] != -1) return dp[n];
-//         int x = solve(n-1,dp);
-//         int y = solve(n-2,dp);
+class Solution {
+public:
+    int solve(int n , vector<int>&dp){
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+        ////
+        if(dp[n] != -1) return dp[n];
+        int x = solve(n-1,dp);
+        int y = solve(n-2,dp);
 
-//         return dp[n] = x+y;
-//     }
-//     int climbStairs(int n) {
+        return dp[n] = x+y;
+    }
+    int climbStairs(int n) {
         
-//         vector<int> dp(n+1,-1);
+        vector<int> dp(n+1,-1);
 
-//         return solve(n , dp);
+        return solve(n , dp);
 
-//     }
-// };
+    }
+};
+
+////t-o(n)
+////s-o(n)
 
 ////////////bottom - up approach -- tabulataion method dp 
 
@@ -61,29 +66,35 @@
 //     }
 // };
 
+/////t-o(n)
+////s-o(n)
+
 ////////////more optimizing -- why don't we take 3 varibles instead 
 ///////// as here at a time we only require previous 2 values only 
 
-class Solution {
-public:
-    int solve(int n , vector<int>&dp){
-        if(n < 0) return 0;
-        if(n <=2) return n;
+// class Solution {
+// public:
+//     int solve(int n , vector<int>&dp){
+//         if(n < 0) return 0;
+//         if(n <=2) return n;
 
-        int a = 1;
-        int b = 2;
-        int c = 3; /////---- can go with 2 ways {1,1} , {2}
-        //////
-        for(int i=3;i<=n;i++){
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return c;
-    }
-    int climbStairs(int n) {
-        vector<int> dp(n+1);
-        return solve(n , dp);
+//         int a = 1;
+//         int b = 2; /////---- can go with 2 ways {1,1} , {2}
+//         int c = 3;
+    
+//         for(int i=3;i<=n;i++){
+//             c = a + b;
+//             a = b;
+//             b = c;
+//         }
+//         return c;
+//     }
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1);
+//         return solve(n , dp);
 
-    }
-};
+//     }
+// };
+
+////t-o(n)
+////s-o(1)
