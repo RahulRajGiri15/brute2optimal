836b8aaf5458ce73b4016e8db3368790cbe0f0df
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 14:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.30%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -14,21 +14,37 @@
 /////t-
 /////s-o(1)---recusive stack - o(n)
 
-/////////using momoization
+/////////using momoization----dynamic programming --top to bottom 
+
+// class Solution {
+// public:
+//     int solve(int n,vector<int>&dp){
+
+//         if(n<=1) return n;
+//         if(dp[n] != -1) return dp[n];
+
+//         return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+
+//     }
+//     int fib(int n) {
+
+//         vector<int> dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+// };
+
+////////////using bottom to top 
 
 class Solution {
 public:
-    int solve(int n,vector<int>&dp){
-
-        if(n<=1) return n;
-        if(dp[n] != -1) return dp[n];
-
-        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
-
-    }
-    int fib(int n) {
-
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+    int fib(int n){
+        if(n <=1) return n;
+        vector<int>dp(n+1,-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
