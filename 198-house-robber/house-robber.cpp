7baa9634c17e06e83d6fbba34a60b5631065138c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-27 17:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (90.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -17,23 +17,50 @@
 //     }
 // };
 
-////////////
+////t-o(2^n)
+////s-o(1)--- recursion stack --- (n)
+
+////////////Dp -- using Memoization 
+
+// class Solution {
+// public:
+//     int solve(vector<int>&nums, int i,vector<int>&dp){
+//             if(i >= nums.size()) return 0;
+            
+//             if(dp[i] != -1) return dp[i];
+//             int pick = nums[i] + solve(nums,i+2,dp);
+//             int not_pick = solve(nums,i+1,dp);
+            
+//             return dp[i]= max(pick , not_pick);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         vector<int>dp(n+1,-1);
+//         return solve(nums,0,dp);
+//     }
+// };
+/////t-o(2^n)
+/////s-o(n)
+
+//////////////
 
 class Solution {
 public:
-    int solve(vector<int>&nums, int i,vector<int>&dp){
-            if(i >= nums.size()) return 0;
+    int t[101];
+    int solve(vector<int>&nums, int i,int n){
+            if(i >= n) return 0;
             
-            if(dp[i] != -1) return dp[i];
-            int pick = nums[i] + solve(nums,i+2,dp);
-            int not_pick = solve(nums,i+1,dp);
+            if(t[i] != -1) return t[i];
+            int pick = nums[i] + solve(nums,i+2,n);
+            int not_pick = solve(nums,i+1,n);
             
-            return dp[i]= max(pick , not_pick);
+            return t[i]= max(pick , not_pick);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int>dp(n+1,-1);
-        return solve(nums,0,dp);
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,n);
     }
 };
