1e54743d153ca129efae6b2b8dca19ae31c22278
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-28 20:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (98.57%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -9,17 +9,51 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void inorder(TreeNode* root,vector<int>& result){
+//         if(root == NULL) return ;
+//         inorder(root->left, result);
+//         result.push_back(root->val);
+//         inorder(root->right,result);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//        vector<int>result;
+//        inorder(root,result);
+//        return result;
+//     }
+// };
+
+///t-o(n)--it visits every element once
+///s- best--balnced tree --height-- log(n) recursive call stack + o(n)--vector
+///s-worst--balanced tree -- o(n)+o(n)=o(n)///height is n;
+
+////////////Morris Traversal
+
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& result){
-        if(root == NULL) return ;
-        inorder(root->left, result);
-        result.push_back(root->val);
-        inorder(root->right,result);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
        vector<int>result;
-       inorder(root,result);
+       TreeNode* curr = root;
+       while(curr != NULL){
+        if(curr->left == NULL){ ///L V R
+            result.push_back(curr->val);
+            curr = curr->right;
+        }
+        else{
+            TreeNode* leftchild = curr->left;
+
+            while(leftchild->right != NULL){
+                leftchild = leftchild->right;
+            }
+            leftchild->right = curr;
+            ////now set your curr left to NULL
+            TreeNode* temp = curr;
+            curr = curr->left;
+            temp->left= NULL;
+        }
+       }
        return result;
     }
 };
+
