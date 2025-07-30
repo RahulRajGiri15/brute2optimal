0d01000be09a8689b333e3efbd13d411cb55da1f
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-07-30 22:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 86.1 MB (38.10%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,29 +1,60 @@
+///////Approach one
+
+// class Solution {
+// public:
+//     int longlength(int idx,vector<int>& nums, int maxand){
+//         ////here we just have to find the subarray with maxm value longest
+//         int maxlen =0;
+//         int curlen =0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i] == maxand){
+//                 curlen++;
+//                 maxlen = max(maxlen,curlen);
+//             }
+//             else{
+//                 curlen=0;
+//             }
+//         }
+//         return maxlen;
+//     }
+//     int longestSubarray(vector<int>& nums) {
+//         ///bitwise AND ---> max bitwise AND is equal to max value present 
+//         int maxand = INT_MIN;
+//         for(int num : nums){
+//             if(num > maxand){
+//                 maxand = num;
+//             }
+//         }
+//         return longlength(0,nums,maxand);
+//     }
+// };
+
+/////t--o(n)
+/////s--o(1)
+
+///////Approach Two
+
 class Solution {
 public:
-    int longlength(int idx,vector<int>& nums, int maxand , int currand){
-        ////here we just have to find the subarray with maxm value longest
-        int maxlen =0;
-        int curlen =0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == maxand){
-                curlen++;
-                maxlen = max(maxlen,curlen);
-            }
-            else{
-                curlen=0;
-            }
-        }
-        return maxlen;
-    }
     int longestSubarray(vector<int>& nums) {
         ///bitwise AND ---> max bitwise AND is equal to max value present 
         int maxand = INT_MIN;
+        int maxlen = 0;
+        int currstreak = 0;
         for(int num : nums){
             if(num > maxand){
                 maxand = num;
+                currstreak = 0;
+                maxlen = 0;
+            }
+            if(num == maxand){
+                currstreak++;
+                maxlen = max(maxlen,currstreak);
+            }
+            else{
+                currstreak = 0;
             }
         }
-        int currand = 0;
-        return longlength(0,nums,maxand,currand);
+        return maxlen;
     }
 };
