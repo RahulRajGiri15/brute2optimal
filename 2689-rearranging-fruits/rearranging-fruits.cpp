20e2d6a07cb5da74b00cd5629cb7ba3c6f00e111
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-02 22:39:19 +0530

    Time: 33 ms (86.90%) | Memory: 90.1 MB (65.52%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.cpp
+++ 2689-rearranging-fruits/rearranging-fruits.cpp
@@ -1,3 +1,45 @@
+// class Solution {
+// public:
+//     long long minCost(vector<int>& basket1, vector<int>& basket2) {
+//         unordered_map<int,int>mp;
+//         int minel = INT_MAX;
+//         for(int &x : basket1){
+//             mp[x]++;
+//             minel = min(x,minel);
+//         }
+//         for(int &x : basket2){
+//             mp[x]--;
+//             minel = min(x,minel);
+//         }
+//         vector<int>finallist;
+//         for(auto &it : mp){
+//             int cost = it.first;
+//             int count = it.second;
+
+//             if(count == 0){
+//                  continue; ////equal num is present on both side
+//             }
+//             if(count %2 != 0){ /// we can't value
+//                 return -1;
+//             }
+//             for(int i=1;i<= abs(count)/2;i++){
+//                 finallist.push_back(cost);
+//             }
+//         }
+//         long long  result = 0;
+//         ////
+//         sort(begin(finallist),end(finallist));
+//         for(int i=0;i<finallist.size()/2;i++){
+//             result += min(finallist[i], minel*2);
+//         }
+
+//         return result;
+//     }
+// };
+
+
+////////////////
+
 class Solution {
 public:
     long long minCost(vector<int>& basket1, vector<int>& basket2) {
@@ -27,58 +69,21 @@ public:
             }
         }
         long long  result = 0;
-        sort(begin(finallist),end(finallist));
+        //////
+        // sort(begin(finallist),end(finallist));
+        // for(int i=0;i<finallist.size()/2;i++){
+        //     result += min(finallist[i], minel*2);
+        // }
+
+        ////////////or
+        ///we don't need to sort all the elment just we just need first half thats all
+        nth_element(begin(finallist),begin(finallist)+finallist.size()/2,end(finallist));
+        ///The element at the middle position (finallist.size()/2) is what would be there if the array was sorted
+
+/////All elements before it are ≤ it, and all after it are ≥ it (but not fully sorted)
         for(int i=0;i<finallist.size()/2;i++){
             result += min(finallist[i], minel*2);
         }
         return result;
     }
 };
-
-// class Solution {
-// public:
-//     long long minCost(vector<int>& basket1, vector<int>& basket2) {
-//         unordered_map<int, int> mp;
-//         int minEl = INT_MAX;
-
-//         for(int &x : basket1) {
-//             mp[x]++;
-//             minEl = min(minEl, x);
-//         }
-
-//         for(int &x : basket2) {
-//             mp[x]--;
-//             minEl = min(minEl, x);
-//         }
-
-//         vector<int> finalList;
-//         for(auto &it : mp) {
-//             int cost  = it.first;
-//             int count = it.second;
-
-//             if(count == 0) {
-//                 continue;
-//             }
-
-//             if(count % 2 != 0) {
-//                 return -1;
-//             }
-
-//             for(int c = 1; c <= abs(count)/2; c++) {
-//                 finalList.push_back(cost);
-//             }
-//         }
-
-//         sort(begin(finalList), end(finalList)); //no need to sort entire array
-//         // nth_element(begin(finalList), begin(finalList)+finalList.size()/2, end(finalList));
-
-//         long long result = 0;
-//         for(int i = 0; i < finalList.size()/2; i++) {
-//             result += min(finalList[i], minEl*2);
-//         }
-
-//         return result;
-
-//     }
-// };
-
