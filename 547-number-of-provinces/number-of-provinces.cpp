83a358c6c32cf7a81cc22293a2d91fb75b510d79
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-02 19:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (13.81%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -35,11 +35,50 @@
 //     }
 // };
 
-///////////
+///////////optimal solution 
+
+// class Solution {
+// public:
+//     void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited,vector<int>& result){
+//         visited[u] = true;
+//         result.push_back(u);
+//         for(auto &v : adj[u]){
+//             if(visited[v] == false){
+//                 DFS(adj,v,visited,result);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int V = isConnected.size();
+//         vector<vector<int>>adj(V);
+//         for(int u=0;u<V;u++){
+//             for(int v=0;v<V;v++){
+//                 if(isConnected[u][v] == 1){
+//                     adj[u].push_back(v);
+//                     adj[v].push_back(u);
+//                 }
+//             }
+//         }
+//         ///now write code for dfs
+//         vector<bool>visited(V,false);
+//         vector<int> result;
+//         int cnt = 0 ; 
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false){
+//                 DFS(adj,i,visited,result);
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+///////////////
+
 
 class Solution {
 public:
-    void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited,vector<int>& result){
+    void DFS(unordered_map<int, vector<int>>& adj,int u,vector<bool>& visited,vector<int>& result){
         visited[u] = true;
         result.push_back(u);
         for(auto &v : adj[u]){
@@ -50,7 +89,8 @@ public:
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V = isConnected.size();
-        vector<vector<int>>adj(V);
+        //vector<vector<int>>adj(V);
+        unordered_map<int, vector<int>>adj;
         for(int u=0;u<V;u++){
             for(int v=0;v<V;v++){
                 if(isConnected[u][v] == 1){
