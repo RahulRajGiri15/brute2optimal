de83c87f7bc6ef0ea274c49ed2a5b6432ab8e446
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-07 22:44:52 +0530

    Time: 69 ms (60.39%) | Memory: 156.1 MB (55.84%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -61,66 +61,112 @@
 
 /////////memoization
 
-#include <algorithm>
+// #include <algorithm>
+// class Solution {
+// public:
+//     int n;
+//     vector<vector<int>>t;
+//     int child1collect(vector<vector<int>>& fruits){
+//         int c1sum=0;
+//         for(int i=0;i<n;i++){
+//             c1sum += fruits[i][i]; /// child 1 can only access diagonal elemnt
+//         }
+//         return c1sum;
+//     }
+//     int child2collect(vector<vector<int>>& fruits,int i,int j){
+//         if(i>=n || j<0 || j>=n){
+//             return 0;
+//         }
+//         if(i == n-1 && j == n-1){
+//             return 0;  /// already collected by child 1
+//         }
+//         if(i == j || i > j){
+//             return 0;
+//         }
+//         if(t[i][j] != -1){
+//             return t[i][j];
+//         }
+//         int curr = fruits[i][j];
+//         int count1 = child2collect(fruits,i+1,j-1);
+//         int count2 = child2collect(fruits,i+1,j);
+//         int count3 = child2collect(fruits,i+1,j+1);
+//         return t[i][j] = curr + max({count1,count2,count3});
+
+//     }
+//      int child3collect(vector<vector<int>>& fruits,int i,int j){
+//         if(i<0 || j>=n || i>=n){
+//             return 0;
+//         }
+//         if(i == n-1 && j == n-1){
+//              return 0;
+//         }
+//         if(i == j || i<j){
+//             return 0;
+//         }
+//         if(t[i][j] != -1){
+//             return t[i][j];
+//         }
+//         int curr = fruits[i][j];
+//         int count1 = child3collect(fruits,i-1,j+1);
+//         int count2 = child3collect(fruits,i,j+1);
+//         int count3 = child3collect(fruits,i+1,j+1);
+//         return t[i][j] = curr + max({count1,count2,count3});
+
+//     }
+//     int maxCollectedFruits(vector<vector<int>>& fruits) {
+//         n = fruits.size();
+//         ///all child can make n-1 moves only 
+//         t.resize(n,vector<int>(n,-1));
+//         int c1 = child1collect(fruits);///can move diagonal only
+//         int c2 = child2collect(fruits,0,n-1);
+//         int c3 = child3collect(fruits,n-1,0);
+        
+//         return c1+c2+c3;
+//     }
+// };
+
+/////////////bottom up////////
+
 class Solution {
 public:
-    int n;
-    vector<vector<int>>t;
-    int child1collect(vector<vector<int>>& fruits){
-        int c1sum=0;
-        for(int i=0;i<n;i++){
-            c1sum += fruits[i][i]; /// child 1 can only access diagonal elemnt
-        }
-        return c1sum;
-    }
-    int child2collect(vector<vector<int>>& fruits,int i,int j){
-        if(i>=n || j<0 || j>=n){
-            return 0;
-        }
-        if(i == n-1 && j == n-1){
-            return 0;  /// already collected by child 1
-        }
-        if(i == j || i > j){
-            return 0;
-        }
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-        int curr = fruits[i][j];
-        int count1 = child2collect(fruits,i+1,j-1);
-        int count2 = child2collect(fruits,i+1,j);
-        int count3 = child2collect(fruits,i+1,j+1);
-        return t[i][j] = curr + max({count1,count2,count3});
-
-    }
-     int child3collect(vector<vector<int>>& fruits,int i,int j){
-        if(i<0 || j>=n || i>=n){
-            return 0;
-        }
-        if(i == n-1 && j == n-1){
-             return 0;
-        }
-        if(i == j || i<j){
-            return 0;
-        }
-        if(t[i][j] != -1){
-            return t[i][j];
-        }
-        int curr = fruits[i][j];
-        int count1 = child3collect(fruits,i-1,j+1);
-        int count2 = child3collect(fruits,i,j+1);
-        int count3 = child3collect(fruits,i+1,j+1);
-        return t[i][j] = curr + max({count1,count2,count3});
-
-    }
     int maxCollectedFruits(vector<vector<int>>& fruits) {
-        n = fruits.size();
-        ///all child can make n-1 moves only 
-        t.resize(n,vector<int>(n,-1));
-        int c1 = child1collect(fruits);///can move diagonal only
-        int c2 = child2collect(fruits,0,n-1);
-        int c3 = child3collect(fruits,n-1,0);
+        int n = fruits.size();
+        vector<vector<int>>t(n,vector<int>(n,0));
+        ///child1collect - diagonal element 
+        int result=0;
+        for(int i=0;i<n;i++){
+            result += fruits[i][i];
+        }
+        ////before child 2 and child 3 nullify all the cells that can't be visited by 2 and 3
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i<j && j+i < n-1){
+                    t[i][j] = 0;
+                }
+                else if(i > j && i+j < n-1){
+                    t[i][j] = 0;
+                }
+                else{
+                    t[i][j] = fruits[i][j];
+                }
+            }
+        }
+
+        ///for child2
+        ////cells uppar to diagonal i < j
+        for(int i=1;i<n;i++){
+            for(int j=i+1;j<n;j++){ //// j = i+1 --- j>i
+                t[i][j] += max({t[i-1][j-1], t[i-1][j],(j+1<n) ?t[i-1][j+1] : 0});
+            }
+        }
+        /////for child 3
+        ////cells lower to diagonal i > j
+        for(int j=1;j<n;j++){
+            for(int i = j+1;i<n;i++){
+                t[i][j] += max({t[i-1][j-1],t[i][j-1],(i+1<n) ?t[i+1][j-1]:0});
+            }
+        }
         
-        return c1+c2+c3;
+        return result + t[n-2][n-1] + t[n-1][n-2];
     }
 };
