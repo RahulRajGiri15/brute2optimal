67b49c6e3f40b900ff32b2f27b84f40481410cf4
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-09 20:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (83.20%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -21,15 +21,31 @@
 
 ////////// method 2 -- store all the power of 2 till INT_MAX
 
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         vector<int>power;
+//         long long v = 1;
+//         while(v < INT_MAX){
+//             power.push_back(int(v));
+//             v *= 2;
+//         }
+//         return find(power.begin(),power.end(),n) != power.end();
+//     }
+// };
+
+///t--o(1)
+///s--o(1)---as 2^32 almost equals INT_MAX
+
+//////////// recursion 
+
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        vector<int>power;
-        long long v = 1;
-        while(v < INT_MAX){
-            power.push_back(int(v));
-            v *= 2;
+        if(n == 1) return true;
+        if(n > 0 && n%2 == 0){
+            return (n & (n-1)) == 0;
         }
-        return find(power.begin(),power.end(),n) != power.end();
+        return false;
     }
 };
