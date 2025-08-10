0dee3f749fd687f2a9586b7dce3bfc21655eb178
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-10 16:29:50 +0530

    Time: 2 ms (34.81%) | Memory: 9.7 MB (14.75%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,17 +1,43 @@
+//////using string problem -- string anagrams
+
+// class Solution {
+// public:
+//     string getsorted(int n){
+//         string s = to_string(n); ///d-digits -- d = 10
+//         sort(s.begin(),s.end()); ///d-digits o(dlogd)--o(10)
+//         return s;
+//     }
+//     bool reorderedPowerOf2(int n) {
+//         string ans = getsorted(n);
+//         for(int p=0;p<30;p++){
+//             if(ans == getsorted(1 << p)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+////t--o(1)
+////s---O(d)
+
+
+///////////
+
 class Solution {
 public:
-    string getsorted(int n){
-        string s = to_string(n);
-        sort(s.begin(),s.end());
-        return s;
-    }
     bool reorderedPowerOf2(int n) {
-        string ans = getsorted(n);
-        for(int p=0;p<30;p++){
-            if(ans == getsorted(1 << p)){
-                return true;
-            }
-        }
+       unordered_set<string>st;
+       for(int p=0;p<=30;p++){
+        string poww = to_string(1 << p);
+        sort(begin(poww),end(poww));
+        st.insert(poww);
+       }
+       string s = to_string(n);
+       sort(begin(s),end(s));
+       if(!st.count(s)){
         return false;
+       }
+       return true;
     }
 };
