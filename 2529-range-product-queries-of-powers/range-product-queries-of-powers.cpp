25258655c08b1dea5b60a5d04c0d2524f9e6da8e
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-11 20:49:55 +0530

    Time: 11 ms (85.11%) | Memory: 147.7 MB (58.51%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
+    int mod = 1e9+7;
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        int mod = 1000000007;
-        vector<int>power;
-        for(int i=0;i<32;i++){
-            if(n & (1<<i)){
+        //int mod = 1000000007;
+        vector<int>power; ///max -o(32)
+        for(int i=0;i<32;i++){  ///o(1)
+            ///if(n & (1<<i)){
+            if((n & (1<<i)) != 0){ /// ith bit is set 
                 power.push_back(1<<i);
             }
         }
         vector<int>ans;
-        for(auto &arr : queries){
+        for(auto &arr : queries){ /// q -- number of queries ///o(qlog n)
             int i = arr[0];
             int j = arr[1];
             long long pro = 1;
-            for(int num=i;num<=j;num++){
+            for(int num=i;num<=j;num++){ ////max -- o(32)
                 pro = (pro * power[num]) % mod;
             }
             //pro = pro % mod;
@@ -22,3 +24,6 @@ public:
         return ans;
     }
 };
+
+///t--o(q)--for query + o(32)-for power -- o(32)-->o(q)
+///s -- o(32)--for power + o(q)--for result--->o(q)
