1311d696e0d8d36b67dc095aed9a69db6f8dd100
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-13 19:47:44 +0530

    Time: 4 ms (47.44%) | Memory: 8.9 MB (46.41%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -17,6 +17,27 @@
 
 ////////
 
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while(n){
+            if(n == 1){
+                return true;
+            }
+            if(n % 3 == 0){
+                n = n/3;
+            }
+            else{
+                return false;
+            }
+        }
+       return false;
+    }
+};
+
+
+////////////
+
 // class Solution {
 // public:
 //     bool isPowerOfThree(int n) {
@@ -33,32 +54,49 @@
 ///t--o(n/2)
 ///s--o(1)
 
-///////////wrong -- for large not work and pow return doubles
+/////////// recursion -1 
 
-class Solution {
-public:
-    bool solve(int i ,int  n){
-        long long  val =1;
-        for(int j=1;j<i;j++){
-            val = val*3;
-        }
-        if(val == n){
-            return true;
-        }
-        if(val > n){
-            return false;
-        }
-        return solve(i+1,n);
-    }
-    bool isPowerOfThree(int n) {
-        if(n <= 0) return false;
-        return solve(1,n);
-    }
-};
+// class Solution {
+// public:
+//     bool solve(int i ,int  n){
+//         long long  val =1;
+//         for(int j=1;j<i;j++){
+//             val = val*3;
+//         }
+//         if(val == n){
+//             return true;
+//         }
+//         if(val > n){
+//             return false;
+//         }
+//         return solve(i+1,n);
+//     }
+//     bool isPowerOfThree(int n) {
+//         if(n <= 0) return false;
+//         return solve(1,n);
+//     }
+// };
 
-////t--o(lo)
-////s--o(1)
-////////////////
+////t--o(log n)
+////s--o(log n)
+
+//////////////// recursion - 2
+
+
+// class Solution {
+// public:
+//     bool solve(long long val ,int  n){
+//         if(val == n) return true;
+//         if(val > n ) return false;
+        
+//         return solve(val * 3 , n);
+//     }
+//     bool isPowerOfThree(int n) {
+//         return solve(1,n);
+//     }
+// };
+
+////////////// other approach other than recursion and loop
 
 
 // class Solution {
