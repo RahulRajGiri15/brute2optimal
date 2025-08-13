22ea7109c5c1241906241f6274d7163bacd3a6fb
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-13 19:49:28 +0530

    Time: 1 ms (73.42%) | Memory: 8.8 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -21,14 +21,16 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         while(n){
-            if(n == 1){
-                return true;
-            }
             if(n % 3 == 0){
                 n = n/3;
             }
             else{
-                return false;
+                if(n == 1){
+                    return true;
+                }
+                else{
+                    return false;
+                }
             }
         }
        return false;
