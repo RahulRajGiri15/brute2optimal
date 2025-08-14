c7184e8fee1ababc344ab46864d77e7e60420f82
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-14 15:30:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (77.37%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -1,20 +1,38 @@
+/////Brute force
+
+// class Solution {
+// public:
+//     string largestGoodInteger(string num) {
+//         string res = "";
+//         int cnt = 0;
+//         for(int i=0;i<=num.size()-3;i++){
+//             if(num[i] == num[i+1] && num[i+1] == num[i+2]){
+//                 if(cnt == 0){
+//                     res = string(3,num[i]);
+//                     cnt++;
+//                 }
+//                 else if(cnt != 0){
+//                     int temp = atoi(string(3,num[i]).c_str());
+//                     if(atoi(res.c_str()) < temp){
+//                         res = to_string(temp);
+//                     }
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+///////////another method
+
 class Solution {
 public:
     string largestGoodInteger(string num) {
         string res = "";
-        int cnt = 0;
         for(int i=0;i<=num.size()-3;i++){
             if(num[i] == num[i+1] && num[i+1] == num[i+2]){
-                if(cnt == 0){
-                    res = string(3,num[i]);
-                    cnt++;
-                }
-                else if(cnt != 0){
-                    int temp = atoi(string(3,num[i]).c_str());
-                    if(atoi(res.c_str()) < temp){
-                        res = to_string(temp);
-                    }
-                }
+                string curr = string(3,num[i]);
+                res = max(res,curr);
             }
         }
         return res;
