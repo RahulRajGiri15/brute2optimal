c015fbad261e3907a720a868ea3a430aed1e11a1
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-16 15:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.57%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -17,19 +17,32 @@
 //     }
 // };
 
-////////////////loop 
+// This already works fine without the n == 2 hack because divisors of 4^15 are only powers of 2,
+// but only powers of 4 should be allowed. That’s the catch.
 
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        long long val = 1;
-        if(n <= 0) return false;
-        while(val < n){
-            val = val*4;
-        }
-        return val == n;
+        ////highest power of 4 in INT_MAX is 4^15 = 1073741824
+        return n > 0 && (n & (n-1)) == 0 && n % 3 == 1;
     }
 };
+
+
+
+////////////////loop 
+
+// class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//         long long val = 1;
+//         if(n <= 0) return false;
+//         while(val < n){
+//             val = val*4;
+//         }
+//         return val == n;
+//     }
+// };
 
 ////////
 
