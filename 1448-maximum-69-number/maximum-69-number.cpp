0248987aa98c6f150f4effc4c0aa30d6c4b0830e
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-16 21:55:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,15 +1,38 @@
+// class Solution {
+// public:
+//     int maximum69Number (int num) {
+//         string ans = to_string(num);
+//         for(int i=0;i<ans.size();i++){
+//             if(ans[i] == '6'){
+//                 ans[i] = '9';
+//                 break;
+//             }
+//         }
+//         // int res = stoi(ans);
+//         // return res;
+//         ////////////////////////
+//         return stoi(ans);
+//     }
+// };
+
+//////////////////////
+
 class Solution {
 public:
     int maximum69Number (int num) {
-        string ans = to_string(num);
-        for(int i=0;i<ans.size();i++){
-            if(ans[i] == '6'){
-                ans[i] = '9';
-                break;
+        int no = num;
+        int pv = -1;
+        int cnt = 0;
+        while(num > 0){
+            int r = num % 10;
+            num = num / 10;
+            if(r == 6){
+                pv = cnt;
             }
+            cnt++;
         }
-        // int res = stoi(ans);
-        // return res;
-        return stoi(ans);
+        if(pv == -1) return no;
+        no = no + (3* pow(10,pv));
+        return no;
     }
 };
