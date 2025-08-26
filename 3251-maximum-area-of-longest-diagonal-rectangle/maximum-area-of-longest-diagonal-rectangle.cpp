31fc228bc73ab863d1b2ca90c817cb8d6696d241
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-26 19:24:51 +0530

    Time: 0 ms (100.00%) | Memory: 29.1 MB (94.05%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+//         int m = dimensions.size();
+//         int n = dimensions[0].size();
+        
+//         float maxdign = -1;
+//         float maxarea = -1;
+
+//         for(int i=0;i<m;i++){
+//             float dig = 0;
+//                 dig = sqrt((dimensions[i][0]*dimensions[i][0]) + (dimensions[i][1]*dimensions[i][1]));
+//                 float area =  dimensions[i][0] * dimensions[i][1];
+
+//                 if(dig > maxdign){
+//                     maxdign = dig;
+//                     maxarea = area;
+//                 }
+//                 else if(dig == maxdign && area > maxarea){
+//                     maxarea = area;
+//                 }
+//         }
+//         return maxarea;
+//     }
+// };
+
+/////t-o(m*n)
+/////s-o(1)
+
+
+//////////////////////
+
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
@@ -8,18 +40,23 @@ public:
         float maxarea = -1;
 
         for(int i=0;i<m;i++){
+            int l = dimensions[i][0];
+            int b = dimensions[i][1];
             float dig = 0;
-                dig = sqrt((dimensions[i][0]*dimensions[i][0]) + (dimensions[i][1]*dimensions[i][1]));
-                float area =  dimensions[i][0] * dimensions[i][1];
+                dig = sqrt(l*l + b*b);
+                float area =  l * b;
 
                 if(dig > maxdign){
                     maxdign = dig;
                     maxarea = area;
                 }
-                else if(dig == maxdign && area > maxarea){
-                    maxarea = area;
+                else if(dig == maxdign){
+                    maxarea = max(maxarea , area);
                 }
         }
         return maxarea;
     }
 };
+
+/////t-o(m*n)
+/////s-o(1)
