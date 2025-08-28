67cd2c1693539a5085523821d5db56c9975aad87
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-28 19:42:58 +0530

    Time: 512 ms (37.91%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -9,8 +9,20 @@
 
 ########OR
 
+-- select w1.id 
+-- from Weather w1 
+-- INNER JOIN weather w2
+-- where DATEDIFF(w1.recordDate,w2.recordDate) = 1
+-- And w1.temperature > w2.temperature 
+
+
+########OR
+
 select w1.id 
 from Weather w1 
 INNER JOIN weather w2
-where DATEDIFF(w1.recordDate,w2.recordDate) = 1
-And w1.temperature > w2.temperature 
+ON SUBDATE(w1.recordDate,1) = W2.recordDate And w1.temperature > w2.temperature 
+
+
+####SUBDATE - it means subtract dates w1 date -1 = w2.date
+####DATEDIFF - it means subtract dates w1 date - w2 date = 1
