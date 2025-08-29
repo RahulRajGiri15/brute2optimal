22d25fed6a82ba79b16371e2ac0dd5efe412e45c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-08-29 20:06:54 +0530

    Time: 866 ms (77.47%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -4,11 +4,11 @@
 #### Here we will use CROSS JOIN 
 ##  In cross join -- we have cartesian product of two tables (every elemnt of table A will be paired with every element of table B) 
 
-select s.student_id, s.student_name,sub.subject_name , count(e.subject_name) as attended_exams
+select s.student_id, s.student_name,su.subject_name , count(e.subject_name) as attended_exams
 from Students s 
-CROSS JOIN Subjects sub 
+CROSS JOIN Subjects su 
 LEFT JOIN Examinations e
-ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+ON s.student_id = e.student_id AND su.subject_name = e.subject_name
 
-Group BY s.student_id , s.student_name,sub.subject_name
+Group BY s.student_id , s.student_name,su.subject_name
 order BY student_id, subject_name
