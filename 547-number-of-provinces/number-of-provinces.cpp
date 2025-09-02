bea16c93bfca58c360fdd464711a59b38acafea3
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-02 19:35:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (57.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -269,14 +269,50 @@
 /////////////
 
 
+// class Solution {
+// public:
+//     int V;
+//     void DFS(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
+//         visited[u] =true;
+//         for(int v=0;v<V;v++){
+//             if(visited[v] == false && isConnected[u][v] == 1){
+//                 DFS(isConnected,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         V = isConnected.size();
+        
+//         vector<bool>visited(V,false);
+//         int count = 0;
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false){
+//                 DFS(isConnected,i,visited);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+/////////////////DFS
+
+
 class Solution {
 public:
     int V;
     void DFS(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
         visited[u] =true;
-        for(int v=0;v<V;v++){
-            if(visited[v] == false && isConnected[u][v] == 1){
-                DFS(isConnected,v,visited);
+        queue<int>que;
+        que.push(u);
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            for(int v =0;v<V;v++){
+                if(visited[v] == false && isConnected[u][v] == 1){
+                    que.push(v);
+                    visited[v] = true;
+                }
             }
         }
     }
