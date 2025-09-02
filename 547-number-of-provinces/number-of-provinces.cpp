cca3eb3fd60fecc3431ac4e16bf20d1be5ffbc49
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-02 18:21:18 +0530

    Time: 3 ms (42.09%) | Memory: 20.8 MB (35.18%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,5 +1,4 @@
 
-
 // class Solution {
 // public:
 //     void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited){
@@ -113,33 +112,156 @@
 //     }
 // };
 
+///t--o(V+E)
+///s--o(v+E)
+
 ///////////////without using any extra space
+
+// class Solution {
+// public:
+//     int V;
+//     void DFS(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
+//         visited[u] = true;
+
+//         for(int v=0; v<V; v++){
+//             if(visited[v] == false && isConnected[u][v] == 1){
+//                 DFS(isConnected,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         V = isConnected.size();
+        
+//         ///now write code for dfs
+//         vector<bool>visited(V,false);
+//         int cnt = 0 ; 
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false){
+//                 DFS(isConnected,i,visited);
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+///t--o(V+E)
+///s--o(V)
+
+
+///////////////////////Using BFS /////////////////
+
+// class Solution {
+// public:
+//     void BFS(vector<vector<int>>& adj,int u,vector<bool>& visited){
+//         visited[u] = true;
+//         queue<int>que;
+//         que.push(u);
+//         while(!que.empty()){
+//             int u = que.front();
+//             que.pop();
+//             for(auto &v : adj[u]){
+//                 if(visited[v] == false){
+//                     que.push(v);
+//                     visited[v] = true;
+//                 }
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//        int  V = isConnected.size();
+//         vector<vector<int>>adj(V);
+//         for(int i=0;i<V;i++){
+//             for(int j=0;j<V;j++){
+//                 if(isConnected[i][j] == 1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         int cnt=0;
+//         vector<bool>visited(V,false);
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false){
+//                 BFS(adj,i,visited);
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+//////////without using extra space /// using BFS
+
+
+// class Solution {
+// public:
+//     void BFS(vector<vector<int>>& isConnected, int u, vector<bool>& visited){
+//         int V = isConnected.size();
+//         visited[u] = true;
+//         queue<int>que;
+//         que.push(u);
+//         while(!que.empty()){
+//             int u = que.front();
+//             que.pop();
+//             for(int v=0; v<V; v++){
+//                 if(visited[v] == false && isConnected[u][v] == 1){
+//                     que.push(v);
+//                     visited[v] = true;
+//                 }
+//             }
+//         }
+//     }
+
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int  V = isConnected.size();
+
+//         int cnt=0;
+//         vector<bool>visited(V,false);
+//         for(int i=0;i<V;i++){
+//             if(visited[i] == false){
+//                 BFS(isConnected,i,visited);
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+////////////////////////////
 
 
 class Solution {
 public:
-    int V;
-    void DFS(vector<vector<int>>& isConnected,int u,vector<bool>& visited){
-        visited[u] = true;
-
-        for(int v=0; v<V; v++){
-            if(visited[v] == false && isConnected[u][v] == 1){
-                DFS(isConnected,v,visited);
+    void DFS(vector<vector<int>>& adj,int u,vector<bool>& visited){
+        visited[u] =true;
+        for(auto &v : adj[u]){
+            if(visited[v] == false){
+                DFS(adj,v,visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        V = isConnected.size();
-        
-        ///now write code for dfs
-        vector<bool>visited(V,false);
-        int cnt = 0 ; 
-        for(int i=0;i<V;i++){
-            if(visited[i] == false){
-                DFS(isConnected,i,visited);
-                cnt++;
+        int V = isConnected.size();
+        vector<vector<int>>adj(V);
+        for(int u=0;u<V;u++){
+            for(int v=0;v<V;v++){
+                if(isConnected[u][v] == 1){
+                    adj[u].push_back(v);
+                    adj[v].push_back(u);
+                }
             }
         }
-        return cnt;
+        vector<bool>visited(V,false);
+        int count = 0;
+        for(int i=0;i<V;i++){
+            if(visited[i] == false){
+                DFS(adj,i,visited);
+                count++;
+            }
+        }
+        return count;
     }
 };
