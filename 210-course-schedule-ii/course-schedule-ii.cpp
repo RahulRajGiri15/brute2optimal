942542dfdb8c2e5a9b3418061e951de5336ee064
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-02 21:24:22 +0530

    Time: 6 ms (37.83%) | Memory: 18.5 MB (51.34%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        //// it is just asking to send topological sort 
+        int n = numCourses;
+        vector<int>indegree(n,0);
+        vector<int>result;
+        //vector<vector<int>>&adj(n);
+        unordered_map<int,vector<int>>adj;
+        for(auto &num : prerequisites){
+            int u = num[0];
+            int v = num[1];
+            ////v--->u
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        queue<int>que;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                que.push(i);
+            }
+        }
+        /////writing bfs
+        while(!que.empty()){
+            int u = que.front();
+            que.pop();
+            result.push_back(u);
+            for(int &v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    que.push(v);
+                }
+            }
+        }
+        return (result.size() == n)? result : vector<int>();
+    }
+};
