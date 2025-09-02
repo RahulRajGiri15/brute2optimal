e6d193d2fda5274adf00743286fc99903665e9e0
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-02 20:18:15 +0530

    Time: 10 ms (25.19%) | Memory: 19.8 MB (50.77%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -46,48 +46,93 @@
 ////////////////topological sorting
 
 
+// class Solution {
+// public:
+
+//     bool topodfs(unordered_map<int,vector<int>>& adj,int numCourses, vector<int>& indegree){
+//         int count = 0;
+//         queue<int> que;
+//         for(int v=0;v<numCourses;v++){
+//             if(indegree[v] == 0){
+//                 que.push(v);
+//             }
+//         }
+//         while(!que.empty()){
+//             int u = que.front();
+//             que.pop();
+//              count++;
+//             for(auto &v : adj[u]){
+//                 indegree[v]--;
+
+//                 if(indegree[v] == 0){
+//                     que.push(v);
+                   
+//                 }
+//             }
+//         }
+//         if(count == numCourses) return true;
+//         return false;
+
+//     }
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         unordered_map<int,vector<int>>adj;
+//         vector<int>indegree(numCourses,0);
+//         for(auto &vec : prerequisites){
+//                 int i = vec[0];
+//                 int j = vec[1];
+//                 ///j--->a
+//                 adj[j].push_back(i);
+//                 indegree[i]++;
+
+//         }
+//         return topodfs(adj,numCourses,indegree);
+        
+//     }
+// };
+
+///////////cylce detection -- topologocal sort
+
 class Solution {
 public:
-
-    bool topodfs(unordered_map<int,vector<int>>& adj,int numCourses, vector<int>& indegree){
-        int count = 0;
-        queue<int> que;
-        for(int v=0;v<numCourses;v++){
-            if(indegree[v] == 0){
-                que.push(v);
+    int n;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>>adj;
+        n = numCourses;
+        vector<int>indegree(n,0);
+        for(auto &num : prerequisites){
+                int u = num[0];
+                int v = num[1];
+                adj[v].push_back(u);
+                indegree[u]++;
+        }
+        
+        // for(int i=0;i<n;i++){
+        //     for(int &t : adj[i]){
+        //         indegree[t]++;
+        //     }
+        // }
+        queue<int>que;
+        for(int i=0;i<n;i++){
+            if(indegree[i] == 0){
+                que.push(i);
             }
         }
+        ///// bfs
+        int count =0;
         while(!que.empty()){
-            int u = que.front();
+            int u= que.front();
             que.pop();
-             count++;
-            for(auto &v : adj[u]){
+            count++;
+            for(auto v : adj[u]){
                 indegree[v]--;
-
                 if(indegree[v] == 0){
                     que.push(v);
-                   
                 }
             }
         }
-        if(count == numCourses) return true;
-        return false;
-
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>>adj;
-        vector<int>indegree(numCourses,0);
-        for(auto &vec : prerequisites){
-                int i = vec[0];
-                int j = vec[1];
-                ///j--->a
-                adj[j].push_back(i);
-                indegree[i]++;
-
+        if(count == n){
+            return true; /// no cycle is present 
         }
-        return topodfs(adj,numCourses,indegree);
-        
+        return false;
     }
 };
-
-///////////cylce detection -- using DFS
