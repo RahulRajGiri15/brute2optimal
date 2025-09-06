ac8f81cc30645d9b1180ec5b2e7e6384b6857f15
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-06 21:44:49 +0530

    Time: 237 ms (60.58%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
 
-Select * 
-from Cinema
-where id%2 != 0 
-    AND description  != 'boring'
-order by rating DESC;
+-- Select * 
+-- from Cinema
+-- where id%2 != 0 
+--     AND description  != 'boring'
+-- order by rating DESC;
+
+-- another way ------- != can be written as <>
+
+SELECT * 
+FROM Cinema 
+Where id%2 <> 0
+    AND description <> 'boring'
+ORDER BY rating DESC;
