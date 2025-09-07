bdd03ce6a17d2127d51fe2f85e4c5ff120ecb218
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-07 13:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (13.58%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -1,14 +1,54 @@
+/////////////method 1 -- 1st approach 
+
+// class Solution {
+// public:
+//     vector<int> sumZero(int n) {
+//         vector<int>res;
+//         if(n %2 != 0){
+//             n = n-1;
+//             res.push_back(0);
+//         }
+//         for(int i=1;i<=n/2;i++){
+//             res.push_back(i);
+//             res.push_back(-i);
+//         }
+//         return res;
+//     }
+// };
+
+//////////////method 2 -- 
+
+// class Solution {
+// public:
+//     vector<int> sumZero(int n) {
+//         vector<int>res(n);
+//         int start = 1;
+//         int i=0;
+//         while(i+1 < n){
+//             res[i] = start;
+//             res[i+1] = -start;
+//             i +=2;
+//             start++;
+//         }
+//         return res;
+//     }
+// };
+
+///////////// method 3 -- 
+
 class Solution {
 public:
     vector<int> sumZero(int n) {
-        vector<int>res;
-        if(n %2 != 0){
-            n = n-1;
-            res.push_back(0);
-        }
-        for(int i=1;i<=n/2;i++){
-            res.push_back(i);
-            res.push_back(-i);
+        vector<int>res(n);
+        int start = 1;
+        int i=0;
+        int j = n-1;
+        while(i < j){
+            res[i] = start;
+            res[j] = -start;
+            i++;
+            j--;
+            start++;
         }
         return res;
     }
