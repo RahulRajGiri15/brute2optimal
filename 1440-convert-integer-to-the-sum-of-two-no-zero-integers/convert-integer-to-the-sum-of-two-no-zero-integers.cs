983b92582d324bda04e0cec735d615c8c7f52fa8
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-08 18:18:33 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (38.71%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cs
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cs
@@ -1,23 +1,64 @@
-public class Solution {
-    bool isNozero(int num){
+// public class Solution {
+//     bool isNozero(int num){
+//         while(num > 0){
+//             if(num %10 == 0) return false;
+//             num = num/10;
+//         }
+//         return true;
+//     }
+//     public int[] GetNoZeroIntegers(int n) {
+//         List<int>ans = new List<int>();
+//         for(int i=1;i<n;i++){
+//             int j = n-i;
+//             if(isNozero(i) && isNozero(j)){
+//                 //return new <int>{a,b};
+//                 ans.Add(i);
+//                 ans.Add(j);
+//                 break;
+//             }
+//         }
+//         Console.WriteLine(String.Join(',',ans));
+//         return ans.ToArray();
+//     }
+// }
+
+/////////////second method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Solution{
+    bool isnozero(int num){
         while(num > 0){
             if(num %10 == 0) return false;
-            num = num/10;
+            num = num /10;
         }
         return true;
     }
-    public int[] GetNoZeroIntegers(int n) {
-        List<int>ans = new List<int>();
+    public int[] GetNoZeroIntegers(int n){
+        List<int> arr = new List<int>();
         for(int i=1;i<n;i++){
             int j = n-i;
-            if(isNozero(i) && isNozero(j)){
-                //return new <int>{a,b};
-                ans.Add(i);
-                ans.Add(j);
-                break;
+            if(isnozero(i) && isnozero(j)){
+                return new int[] {i , j};
             }
         }
-        Console.WriteLine(String.Join(',',ans));
-        return ans.ToArray();
+        return new int[0];
     }
 }
