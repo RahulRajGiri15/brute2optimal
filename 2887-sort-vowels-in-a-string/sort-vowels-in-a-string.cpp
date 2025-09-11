a9980c3b7d9aa7388700f2ccbbf84ae6e1ceb33c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-11 22:38:40 +0530

    Time: 19 ms (35.71%) | Memory: 15.7 MB (85.82%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//      bool isVowel(char c){
+//         c = tolower(c);
+//         if(c == 'a' || c == 'e' || c == 'i' || c=='o' || c == 'u'){
+//             return true;
+//         }
+//         return false;
+//         }
+//     string sortVowels(string s) {
+//         string temp ;
+//         for(char &c : s){
+//             if(isVowel(c)){
+//                 temp.push_back(c);
+//             }
+//         }
+//         sort(temp.begin(),temp.end());
+//         int k=0;
+//         for(int i=0;i<s.size();i++){
+//             if(isVowel(s[i])){
+//                 s[i] = temp[k];
+//                 k++;
+//             }
+//         }
+//         return s;
+//     }
+// };
+
+
+///////////////////////
+
+
 class Solution {
 public:
      bool isVowel(char c){
@@ -8,20 +40,30 @@ public:
         return false;
         }
     string sortVowels(string s) {
-        string temp ;
+        unordered_map<char, int>mp;
         for(char &c : s){
             if(isVowel(c)){
-                temp.push_back(c);
+                mp[c]++;
             }
         }
-        sort(temp.begin(),temp.end());
-        int k=0;
+        string temp = "AEIOUaeiou";
+        int j =0;
         for(int i=0;i<s.size();i++){
             if(isVowel(s[i])){
-                s[i] = temp[k];
-                k++;
+                while(mp[temp[j]] == 0){
+                    j++;
+                }
+                s[i] = temp[j];
+                mp[temp[j]]--;
             }
         }
         return s;
     }
 };
+
+
+
+
+
+
+
