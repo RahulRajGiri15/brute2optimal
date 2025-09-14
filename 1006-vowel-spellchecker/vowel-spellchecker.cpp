f3452fbef2ead5cd7d048488bb7c2ea0566f1aa9
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-15 00:17:07 +0530

    Time: 35 ms (83.54%) | Memory: 42.5 MB (33.54%) - LeetSync

--- 1006-vowel-spellchecker/vowel-spellchecker.cpp
+++ 1006-vowel-spellchecker/vowel-spellchecker.cpp
@@ -1,3 +1,75 @@
+// class Solution {
+// public:
+//     unordered_set<string>exactset;
+//     unordered_map<string,string>casemp;
+//     unordered_map<string,string>vowelmp;
+
+//     string tolowerr(string s){
+//         string r ="";
+//         for(char &c : s){
+//             r.push_back(tolower(c));
+//         }
+//         return r;
+//     }
+//     string tovowel(string s){
+//         string r ="";
+//         for(char &c : s){
+//             if(c == 'a'|| c == 'e'|| c == 'i'|| c == 'o'|| c == 'u'){
+//                 c = '*';
+//                 r.push_back(c);
+//             }
+//             else{
+//                 r.push_back(c);
+//             }
+            
+//         }
+//         return r;
+//     }
+//     string checkforpri(string query){
+//         if(exactset.count(query)){
+//             return query;
+//         }
+//         string lowercase = tolowerr(query);
+//         if(casemp.count(lowercase)){
+//             return casemp[lowercase];
+//         }
+//         string vowelcase = tovowel(lowercase);
+//         if(vowelmp.count(vowelcase)){
+//             return vowelmp[vowelcase];
+//         }
+//         return "";
+//     }
+
+
+//     vector<string> spellchecker(vector<string>& wordlist, vector<string>& queries) {
+//         exactset.clear();
+//         casemp.clear();
+//         vowelmp.clear();
+
+//         for(auto word : wordlist){
+//             ///filling in set
+//             exactset.insert(word);
+//             string lowercase = tolowerr(word);
+//             ///filling in map
+//             if(casemp.find(lowercase) == casemp.end()){
+//                 casemp[lowercase] = word;
+//             }
+//             ////filling in vowel
+//             string vowelcase = tovowel(lowercase);
+//             if(vowelmp.find(vowelcase) == vowelmp.end()){
+//                 vowelmp[vowelcase] = word;
+//             }
+//         }
+//         vector<string>result;
+//         for(auto &query : queries){
+//             result.push_back(checkforpri(query));
+//         }
+//         return result;
+//     }
+// };
+
+/////////////////////or
+
 class Solution {
 public:
     unordered_set<string>exactset;
@@ -5,25 +77,21 @@ public:
     unordered_map<string,string>vowelmp;
 
     string tolowerr(string s){
-        string r ="";
-        for(char &c : s){
-            r.push_back(tolower(c));
+        string result = s;
+        for(char &c : result){
+            c = tolower(c);
         }
-        return r;
+        return result;
     }
     string tovowel(string s){
-        string r ="";
-        for(char &c : s){
+        string result = s;
+        for(char &c : result){
             if(c == 'a'|| c == 'e'|| c == 'i'|| c == 'o'|| c == 'u'){
                 c = '*';
-                r.push_back(c);
+                
             }
-            else{
-                r.push_back(c);
-            }
-            
         }
-        return r;
+        return result;
     }
     string checkforpri(string query){
         if(exactset.count(query)){
