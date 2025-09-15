d3bec55b6021ece43b8ebbebfc845025b121bcc7
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-15 18:38:22 +0530

    Time: 569 ms (54.50%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,18 +1,35 @@
 # Write your MySQL query statement below
 
-select
-Round(Count(*) / (Select count(Distinct player_id) from Activity) , 2) 
-As fraction
 
-from(
+-- select
+-- Round(Count(*) / (Select count(Distinct player_id) from Activity) , 2) 
+-- As fraction
+
+-- from(
+--     select
+--     a.player_id 
+--     from Activity a
+--     JOIN(
+--         select player_id, min(event_date) as first_date
+--         from Activity 
+--         Group BY player_id
+--     ) b
+--     on b.player_id = a.player_id
+--     and a.event_date = DATE_ADD(first_date , interval 1 Day)
+-- ) as s;
+
+
+-- -----------------------------
+
+select Round(count(player_id) / (select count(distinct player_id) from Activity) ,2)  AS fraction
+
+from Activity
+where(player_id, DATE_SUB(event_date , INTERVAL 1 DAY)) in (
     select
-    a.player_id 
+    player_id,
+    min(event_date) as first_date
     from Activity a
-    JOIN(
-        select player_id, min(event_date) as first_date
-        from Activity 
-        Group BY player_id
-    ) b
-    on b.player_id = a.player_id
-    and a.event_date = DATE_ADD(first_date , interval 1 Day)
-) as s;
+    Group By player_id
+) 
+
+
