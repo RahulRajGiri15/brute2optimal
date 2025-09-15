c87bc685120b44802e9728555bd45f43f169e862
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-15 21:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (69.10%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,33 +1,62 @@
+// class Solution {
+// public:
+//     int canBeTypedWords(string text, string brokenLetters) {
+//         vector<string>st;
+//         string r =""; 
+//         for(auto c : text){
+//             if(c == ' '){
+//                 st.push_back(r);
+//                 r = "";
+//                 continue;
+//             }
+//             r.push_back(c);
+//         }
+//         st.push_back(r);
+        
+//         int count =0;
+//         for(auto &it : st){
+//             bool canform = true;
+//             for(auto c : brokenLetters){
+//                 for(char wc : it){
+//                     if(c == wc){
+//                         canform = false;
+//                         break;
+//                     }
+//                 }
+//             }
+//             if(canform == true){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+//////////////////////////////most optimal Solution 
+
 class Solution {
 public:
     int canBeTypedWords(string text, string brokenLetters) {
-        vector<string>st;
-        string r =""; 
-        for(auto c : text){
-            if(c == ' '){
-                st.push_back(r);
-                r = "";
-                continue;
-            }
-            r.push_back(c);
+        bool mp[26];
+        for(char &c : brokenLetters){
+            mp[c-'a'] = true;
         }
-        st.push_back(r);
-        
-        int count =0;
-        for(auto &it : st){
-            bool canform = true;
-            for(auto c : brokenLetters){
-                for(char wc : it){
-                    if(c == wc){
-                        canform = false;
-                        break;
-                    }
+        bool cantype = true;
+        int result =0;
+        for(char &ch : text){
+            if(ch == ' '){ //////first word finished
+                if(cantype){
+                    result++;
                 }
+                cantype = true;
             }
-            if(canform == true){
-                count++;
+            else if(mp[ch-'a'] == true){
+                cantype = false;
             }
         }
-        return count;
+        if(cantype){
+            result++;
+        }
+        return result;
     }
 };
