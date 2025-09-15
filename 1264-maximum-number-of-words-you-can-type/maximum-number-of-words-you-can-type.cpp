c10ee84814c45f2a36543b071671e236a759517a
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-15 15:54:06 +0530

    Time: 3 ms (18.94%) | Memory: 9.5 MB (21.59%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<string>st;
+        string r =""; 
+        for(auto c : text){
+            if(c == ' '){
+                st.push_back(r);
+                r = "";
+                continue;
+            }
+            r.push_back(c);
+        }
+        st.push_back(r);
+        
+        int count =0;
+        for(auto &it : st){
+            bool canform = true;
+            for(auto c : brokenLetters){
+                for(char wc : it){
+                    if(c == wc){
+                        canform = false;
+                        break;
+                    }
+                }
+            }
+            if(canform == true){
+                count++;
+            }
+        }
+        return count;
+    }
+};
