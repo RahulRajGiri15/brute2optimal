43378378e9d3c876c8dd209a2e24562010d56701
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-17 19:52:15 +0530

    Time: 17 ms (71.43%) | Memory: 125 MB (82.95%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int>result;
+        for(auto num : nums){
+            while(!result.empty()){
+                int prev = result.back();
+                int curr = num;
+                int GCD = gcd(prev,curr);
+                if(GCD == 1){
+                    break;
+                }
+                result.pop_back();
+                int lcm = prev / GCD * curr;
+                num = lcm;
+            }
+            result.push_back(num);
+        }
+        return result;
+    }
+};
