c8664bb1359d51c1b19bc5d36db5d2a192d12818
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-21 16:47:42 +0530

    Time: 1 ms (19.13%) | Memory: 22.9 MB (10.69%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,12 +1,37 @@
+///////brtue force
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         unordered_set<int>st(nums.begin(),nums.end());
+//         nums.clear();
+//         for(auto &it : st){
+//             nums.push_back(it);
+//         }
+//         sort(begin(nums),end(nums));
+//         return nums.size();
+//     }
+// };
+
+///////////////
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_set<int>st(nums.begin(),nums.end());
-        nums.clear();
-        for(auto &it : st){
-            nums.push_back(it);
-        }
-        sort(begin(nums),end(nums));
-        return nums.size();
+       vector<int>temp;
+       int i=0;
+       int j= i+1;
+       int n = nums.size();
+       temp.push_back(nums[i]);
+       while(i < j && j< n){
+            if(nums[i] != nums[j]){
+                temp.push_back(nums[j]);
+                i = j;
+            }
+            j++;
+       }
+       nums.clear();
+       nums.assign(temp.begin(),temp.end());
+       return nums.size();
     }
 };
