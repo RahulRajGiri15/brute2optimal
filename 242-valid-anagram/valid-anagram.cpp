c151cef84440976f1f6a594a5472d5da8fffd608
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-22 10:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
         unordered_map<char,int>mp1;
         unordered_map<char,int>mp2;
         for(auto &c : s){
@@ -9,11 +10,11 @@ public:
         for(auto &c : t){
             mp2[c]++;
         }
-        int n1 = mp1.size();
-        int n2 = mp2.size();
-        if(n1 != n2){
-            return false;
-        }
+        // int n1 = mp1.size();
+        // int n2 = mp2.size();
+        // if(n1 != n2){
+        //     return false;
+        // }
         for(auto &it : mp1){
             if(mp2[it.first] != it.second){
                 return false;
