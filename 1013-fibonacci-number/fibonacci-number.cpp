dfd3037d42400a84909fdf41e08d7046881d93b0
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-22 10:28:21 +0530

    Time: 2431 ms (22.42%) | Memory: 364.6 MB (11.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -77,14 +77,33 @@
 
 ///////////////////////////////////////////////////
 
+// class Solution {
+// public:
+//     int fib(int n) {
+
+//         if(n ==1) return 1;
+//         if(n == 0) return 0;
+
+//         return fib(n-1) + fib(n-2);
+        
+//     }
+// };
+
+////////
+
 class Solution {
 public:
-    int fib(int n) {
+    int solve(int n,vector<int>&dp) {
 
         if(n ==1) return 1;
         if(n == 0) return 0;
+        if(dp[n] != -1 ) return dp[n];
 
-        return fib(n-1) + fib(n-2);
+        return dp[n] = fib(n-1) + fib(n-2);
         
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
