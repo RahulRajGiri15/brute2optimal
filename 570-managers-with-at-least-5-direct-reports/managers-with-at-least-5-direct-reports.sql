d691c6a3915656ac785ab6cc831db2b32912d43c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-23 21:18:04 +0530

    Time: 319 ms (85.93%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,20 @@
 # Write your MySQL query statement below
 
+
+-- select e1.name
+-- from Employee e1
+-- INNER JOIN Employee e2
+-- ON e1.id = e2.managerId
+-- Group BY e2.managerId
+-- HAVING count(e2.managerId) >= 5;
+
+
+####After INNER JOIN why we group by managerId?
+### because we want to put all the rows with same managerId into same row(bucket)
+
 select e1.name
 from Employee e1
-INNER JOIN Employee e2
-ON e1.id = e2.managerId
-Group BY e2.managerId
-HAVING count(e2.managerId) >= 5;
+JOIN Employee e2
+on e1.id = e2.managerId
+Group by e1.id
+Having count(e2.managerId) >= 5
