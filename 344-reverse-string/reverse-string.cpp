fa24053a592376d4606f6a287825ee1fa5961a9a
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-09-26 11:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (5.45%) - LeetSync

---
+++ 344-reverse-string/reverse-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        vector<char> s2;
+        int n = s.size();
+        for(int i=n-1;i>=0;i--){
+            s2.push_back(s[i]);
+        }
+        for(int i=0;i<n;i++){
+            s[i] = s2[i];
+        }
+
+    }
+};
