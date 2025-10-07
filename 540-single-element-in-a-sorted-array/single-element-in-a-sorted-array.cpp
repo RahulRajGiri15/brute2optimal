83ea650664014777ef952ac9e50dfb7e61e1a87e
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-07 21:27:35 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.57%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,16 +1,36 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         unordered_map<int ,int> mp;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]]++;
+//         }
+//         for(auto &it : mp){
+//             if(it.second == 1){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+/////////////////
+
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int ,int> mp;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
+        if(n == 1){
+            return nums[0];
         }
-        for(auto &it : mp){
-            if(it.second == 1){
-                return it.first;
+        int i=0;
+        while(i < n-1){
+            if(nums[i] != nums[i+1]){
+                break;
             }
+            i = i+2;
         }
-        return -1;
+        return nums[i];
     }
 };
