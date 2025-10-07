0a7771a8e43b327117502deb09fe07be50fe246b
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-07 22:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (34.05%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -58,20 +58,59 @@
 
 /////////////////////
 
+/////////o(n)
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n == 1){
+//             return nums[0];
+//         }
+//         int x = nums[0];
+//         int i=1;
+//         while(i < n){
+//             x ^= nums[i];
+//             i++;
+//         }
+//         return x;
+//     }
+// };
+
+//////////////////////////binary search
 
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1){
-            return nums[0];
+        int l = 0;
+        int h = n-1;
+        while(l < h){
+            int mid = l + (h-l)/2;
+            bool iseven;
+            if((h - mid) %2 == 0){ /////right side is even
+                iseven = true;
+            }
+            else{
+                iseven = false; /////right side is odd
+            }
+
+            if(nums[mid] == nums[mid+1]){
+                if(iseven){ ///// right side me odd bachee -- our ans is in right side
+                    l = mid+2;
+                }
+                else{ /////right side me even num of element bachee  -- our ans is in left side
+                    h = mid-1;
+                }
+            }
+            else if(nums[mid] != nums[mid+1]){
+                if(iseven){ ////right side is even -- than every elem has already a pair right 
+                    h  =  mid;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
         }
-        int x = nums[0];
-        int i=1;
-        while(i < n){
-            x ^= nums[i];
-            i++;
-        }
-        return x;
+        return nums[h]; ///////or nums[h]
     }
 };
