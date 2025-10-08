9464d967745ca6c82769a859d835b93de669e092
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-09 02:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.24%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,70 @@
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(matrix[i][j] == target){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+///////////////////////
+
+
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int i = 0 , j = n-1;
+        while(i <m && j >= 0){
+    
+            if(matrix[i][j] > target ){
+                j--;
             }
+            else if(matrix[i][j] < target){
+                i++;
+            }
+            else{
+                return true;
+            }
+
         }
         return false;
     }
 };
+
+
+
+////////////////////////
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         int l = 0 , h = m*n -1;
+//         while(l <= h){
+//             int mid = l + (h-l)/2;
+//             ///// row -> mid/n;
+//             ///// col -> mid%n;
+//             if(matrix[mid/n][mid%n] > target ){
+//                 h = mid -1;
+//             }
+//             else if(matrix[mid/n][mid%n] < target){
+//                 l = mid+1;
+//             }
+//             else{
+//                 return true;
+//             }
+
+//         }
+//         return false;
+//     }
+// };
