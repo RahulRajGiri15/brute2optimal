7e587c0dedf3bcb8af83fbe4a6032aabc97fd81a
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-09 02:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (36.19%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -24,24 +24,56 @@
 
 ///////////////////////////////
 
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int,int>mp;
+//         for(int no : nums){
+//             mp[no]++;
+//         }
+//         vector<pair<int,int>>freq;
+
+//         for(auto &it : mp){
+//             freq.push_back({it.second,it.first});
+//         }
+//         sort(begin(freq),end(freq),greater<>()); ////ascending order
+
+//         vector<int>ans;
+
+//         for(int i=0;i<k;i++){
+//             ans.push_back(freq[i].second);
+//         }
+//         return ans;
+//     }
+// };
+
+////////////////////using min heap
+
+
 class Solution {
 public:
+    typedef pair<int,int> p;
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>mp;
         for(int no : nums){
             mp[no]++;
         }
-        vector<pair<int,int>>freq;
+        //priority_queue<int,vector<int>,greater<int>> pq; ///// min heap
+        priority_queue<p,vector<p>,greater<p>>pq;
 
         for(auto &it : mp){
-            freq.push_back({it.second,it.first});
+            int first = it.first;
+            int second = it.second;
+            pq.push({second,first});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
         }
-        sort(begin(freq),end(freq),greater<>());
-
         vector<int>ans;
-
-        for(int i=0;i<k;i++){
-            ans.push_back(freq[i].second);
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }
