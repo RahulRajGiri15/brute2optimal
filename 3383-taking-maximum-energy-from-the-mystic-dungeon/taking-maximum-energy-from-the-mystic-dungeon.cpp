e874171741cfe3a178081a4f70299bfac847a00f
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-11 00:37:38 +0530

    Time: 165 ms (9.86%) | Memory: 158.1 MB (9.15%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,13 +1,39 @@
+// class Solution {
+// public:
+//     int maximumEnergy(vector<int>& energy, int k) {
+//         int n = energy.size();
+//         int maxsum = INT_MIN;
+//         for(int i=0;i<energy.size();i++){
+//             int sum =0;
+//             int j = i;
+//             while(j < n){
+//                 sum = sum + energy[j];
+//                 j = j+k;
+//             }
+//             maxsum = max(maxsum,sum);
+//         }
+//         return maxsum;
+//     }
+// };
+
+
+////////////////////////
+
 class Solution {
 public:
-    int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        vector<int> dp(n);
-        int result = INT_MIN;
-        for (int i = n - 1; i >= 0; --i) {
-            dp[i] = energy[i] + ((i + k < n) ? dp[i + k] : 0);
-            result = max(result, dp[i]);
+    int n ;
+    int solve(vector<int>& energy,int i,int k,vector<int>&dp){
+                if(i >= n) return 0;
+                if(dp[i] != INT_MIN) return dp[i];
+                return dp[i] = energy[i] + solve(energy,i+k,k,dp);
         }
-        return result;
+    int maximumEnergy(vector<int>& energy, int k) {
+        n = energy.size();
+        int maxsum = INT_MIN;
+        vector<int>dp(n,INT_MIN);
+        for(int i=0;i<energy.size();i++){
+            maxsum = max(maxsum , solve(energy,i,k,dp));
+        }
+        return maxsum;
     }
 };
