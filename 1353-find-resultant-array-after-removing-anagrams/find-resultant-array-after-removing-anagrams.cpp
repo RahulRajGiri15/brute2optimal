88dd94ac5bdaf99ff26e1df4d9be5cb6a297b429
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-13 21:56:09 +0530

    Time: 3 ms (48.84%) | Memory: 17.9 MB (22.11%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
-    bool anagrm(string s , string r){
-        sort(begin(s),end(s));
-        sort(begin(r),end(r));
-        if(s == r){
-            return true;
+    int n ;
+    /////////////1
+    // bool anagrm(string s , string r){
+    //     sort(begin(s),end(s));
+    //     sort(begin(r),end(r));
+    //     if(s == r){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    ////////////2
+     bool anagrm(string s , string r){
+        vector<int>check(26,0);
+        for(int i=0;i<s.size();i++){
+            check[s[i]-'a']++;
         }
-        return false;
+        for(int i=0;i<r.size();i++){
+            check[r[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(check[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
     vector<string> removeAnagrams(vector<string>& words) {
         vector<string>result;
         result.push_back(words[0]);
-        int n = words.size();
+        n = words.size();
         for(int i=1;i<n;i++){
             if(anagrm(words[i],result.back()) == false){
                 result.push_back(words[i]);
