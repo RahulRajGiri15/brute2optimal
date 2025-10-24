20739b0594ef814c5f5f064bc4b895849f327633
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-24 21:18:31 +0530

    Time: 527 ms (33.67%) | Memory: 254.8 MB (22.61%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     bool isbalanced(int num){
+//         vector<int>count(10,0);
+//         while(num >0){
+//             int d = num % 10;
+//             count[d]++;
+//             num = num / 10;
+//         }
+//         for(int i=0;i<10;i++){
+//             if(count[i] != 0 && count[i] != i){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     int nextBeautifulNumber(int n) {
+//         int maxx = pow(10,7);
+//         for(int i = n+1 ; i<maxx ;i++){
+//             if(isbalanced(i)){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+//////////////////////////////////////
+
+////if the n = 10^6 than the maximum perfect number one can get is 1224444
+
 class Solution {
 public:
     bool isbalanced(int num){
@@ -15,8 +46,8 @@ public:
         return true;
     }
     int nextBeautifulNumber(int n) {
-        int maxx = pow(10,7);
-        for(int i = n+1 ; i<maxx ;i++){
+        //int maxx = pow(10,7);
+        for(int i = n+1 ; i<=1224444 ;i++){
             if(isbalanced(i)){
                 return i;
             }
