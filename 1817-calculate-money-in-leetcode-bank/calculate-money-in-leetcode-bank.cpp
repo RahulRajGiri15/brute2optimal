95ce22cf2dfe9e82b0fa8233aa6172a4282e1a91
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-26 18:39:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.94%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -45,6 +45,32 @@
 
 /////////////////////////////////using ap concept
 
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         int result =0;
+        
+//         int totalweeks = n / 7;
+//         int reminingdays = n % 7;
+
+//         int firstel = 28; /// first week sum
+//         int lastweekel = firstel +(totalweeks - 1) * 7;  /////// last = a + (n-1)*d
+
+//         result = totalweeks * (firstel + lastweekel)/2; ////sum = n*(a1 + al)/2;
+
+//         ///for remaining days 
+
+//         int startel = 1 + totalweeks;
+//         for(int i =1;i<= reminingdays;i++){
+//             result += startel;
+//             startel++;
+//         }
+//         return result;
+//     }
+// };
+
+//////////////////////////
+
 class Solution {
 public:
     int totalMoney(int n) {
@@ -61,12 +87,9 @@ public:
         ///for remaining days 
 
         int startel = 1 + totalweeks;
-        for(int i =1;i<= reminingdays;i++){
-            result += startel;
-            startel++;
-        }
+        int lastel = startel + (reminingdays - 1) * 1;
+        result += reminingdays * (startel + lastel)/2;
+
         return result;
-
-
     }
 };
