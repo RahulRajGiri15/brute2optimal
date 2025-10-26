c99306cf883899eaa79ce3e6d4226cb4d2c4cc56
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-26 18:21:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.55%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,26 +1,44 @@
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         int i = 0;
+//         int sum = 0;
+//         int count = 0;
+//         int week = 0;
+//         if(n <= 7){
+//             for(int i = 1;i<=n;i++){
+//                 sum += i;
+//             }
+//         }
+
+//         else if(n > 7){
+//             while(count < n){
+//                 for(int k=1; k<=7 && count < n; k++){
+//                     /////every week is 1 + previous value
+//                     sum += (k + week);
+//                     count++;
+//                 }
+//                 week++;
+//             }
+//         }
+//         return sum;
+//     }
+// };
+
+////////////////////
+
 class Solution {
 public:
     int totalMoney(int n) {
-        int i = 0;
-        int sum = 0;
-        int count = 0;
-        int week = 0;
-        if(n <= 7){
-            for(int i = 1;i<=n;i++){
-                sum += i;
+        int result =0;
+        int week =0;
+        while(n > 0){
+            for(int i=1;i<= min(n,7);i++){
+                result += i + week;
             }
+            n = n-7;
+            week++;
         }
-
-        else if(n > 7){
-            while(count < n){
-                for(int k=1; k<=7 && count < n; k++){
-                    /////every week is 1 + previous value
-                    sum += (k + week);
-                    count++;
-                }
-                week++;
-            }
-        }
-        return sum;
+        return result;
     }
 };
