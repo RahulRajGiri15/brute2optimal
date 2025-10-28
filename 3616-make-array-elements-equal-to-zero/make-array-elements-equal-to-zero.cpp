f8843ecc3e0daef8348e12fa3cdb54a2250eb9e8
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-28 22:04:24 +0530

    Time: 199 ms (5.05%) | Memory: 25.6 MB (6.88%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool check(vector<int>nums,int count , int i ,int dirn){
+        int idx = i;
+        vector<int> temp = nums;
+        while(idx >= 0 && idx < nums.size() && count > 0){
+            if(temp[idx] > 0){
+                temp[idx]--;
+                dirn *= -1;
+                if(temp[idx] == 0){
+                    count--;
+                }
+            } 
+            idx += dirn;
+        }
+        if(count == 0){
+            return true;
+        }
+        return false;
+    }
+    int countValidSelections(vector<int>& nums) {
+        int count =0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i] != 0){
+                count++;
+            }
+        }
+        int result =0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                if(check(nums,count,i,-1)){ /// for left direction
+                    result++;
+                }
+                if(check(nums,count,i,1) == true){ /// for right direction
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+};
