3767f0b22bd8646e6ca2309f51b5db1f848f376c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-30 22:10:51 +0530

    Time: 8 ms (14.25%) | Memory: 81 MB (6.07%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,3 +1,31 @@
+
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int n = target.size();
+        vector<int>diff(n);
+        int curr = 0;
+        int prev = 0;
+        int oper = 0;
+        for(int i=0;i<n;i++){
+            curr = target[i];
+            /// check for sign change --- there will be no sign change as only increment is given
+            /////if there is no case of sign change
+            if(abs(curr) > abs(prev)){
+                oper += abs(curr - prev);
+            }
+            // elseif(abs(curr) <= abs(prev)){
+            //     do nothing; -- because it is already included
+            // }
+            prev = curr;
+        }
+        return oper;
+    }
+};
+
+
+
+////////////////////////////////////////////
 // class Solution {
 // public:
 //     int minNumberOperations(vector<int>& target) {
@@ -25,29 +53,32 @@
 //     }
 // };
 
-class Solution {
-public:
-    int minNumberOperations(vector<int>& target) {
-        int n = target.size();
-        vector<int>diff(n);
-        int curr = 0;
-        int prev = 0;
-        int oper = 0;
-        for(int i=0;i<n;i++){
-            curr = target[i];
-            /// check for sign change
-            if((curr < 0 && prev > 0) || (curr > 0 && prev <0)){
-                oper += abs(curr);
-            }
-            /////if there is no case of sign change
-            else if(abs(curr) > abs(prev)){
-                oper += abs(curr - prev);
-            }
-            // elseif(abs(curr) <= abs(prev)){
-            //     do nothing; -- because it is already included
-            // }
-            prev = curr;
-        }
-        return oper;
-    }
-};
+/////////////////////////////////////////
+
+
+// class Solution {
+// public:
+//     int minNumberOperations(vector<int>& target) {
+//         int n = target.size();
+//         vector<int>diff(n);
+//         int curr = 0;
+//         int prev = 0;
+//         int oper = 0;
+//         for(int i=0;i<n;i++){
+//             curr = target[i];
+//             /// check for sign change
+//             if((curr < 0 && prev > 0) || (curr > 0 && prev <0)){
+//                 oper += abs(curr);
+//             }
+//             /////if there is no case of sign change
+//             else if(abs(curr) > abs(prev)){
+//                 oper += abs(curr - prev);
+//             }
+//             // elseif(abs(curr) <= abs(prev)){
+//             //     do nothing; -- because it is already included
+//             // }
+//             prev = curr;
+//         }
+//         return oper;
+//     }
+// };
