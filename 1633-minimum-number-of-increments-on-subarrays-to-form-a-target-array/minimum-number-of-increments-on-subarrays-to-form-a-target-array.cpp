7553e98413d6f64b262a80b9511904c03aeed0b5
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-30 22:06:10 +0530

    Time: 0 ms (100.00%) | Memory: 81 MB (5.22%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,3 +1,30 @@
+// class Solution {
+// public:
+//     int minNumberOperations(vector<int>& target) {
+//         int n = target.size();
+//         vector<int>diff(n);
+//         int curr = 0;
+//         int prev = 0;
+//         int oper = 0;
+//         for(int i=0;i<n;i++){
+//             curr = target[i];
+//             /// check for sign change
+//             if(curr < 0 && prev > 0){
+//                 oper += abs(curr);
+//             }
+//             if(curr > 0 && prev <0){
+//                 oper += abs(curr);
+//             }
+//             /////if there is no case of sign change
+//             else if(abs(curr) > abs(prev)){
+//                 oper += abs(curr - prev);
+//             }
+//             prev = curr;
+//         }
+//         return oper;
+//     }
+// };
+
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
@@ -9,16 +36,16 @@ public:
         for(int i=0;i<n;i++){
             curr = target[i];
             /// check for sign change
-            if(curr < 0 && prev > 0){
-                oper += abs(curr);
-            }
-            if(curr > 0 && prev <0){
+            if((curr < 0 && prev > 0) || (curr > 0 && prev <0)){
                 oper += abs(curr);
             }
             /////if there is no case of sign change
             else if(abs(curr) > abs(prev)){
                 oper += abs(curr - prev);
             }
+            // elseif(abs(curr) <= abs(prev)){
+            //     do nothing; -- because it is already included
+            // }
             prev = curr;
         }
         return oper;
