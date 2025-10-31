f6e0370d9ecd258d20c221fd36f44f6a426b7d03
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-31 21:55:38 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (95.85%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,22 +1,64 @@
-class Solution {
-public:
-    vector<int> getSneakyNumbers(vector<int>& nums) {
-        sort(begin(nums) , end(nums));
-        int a =0 ,b =0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]){
-                b = a;
-                a = nums[i];
-            }
-        }
-        return {a,b};
-    }
-};
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         sort(begin(nums) , end(nums));
+//         int a =0 ,b =0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i] == nums[i+1]){
+//                 b = a;
+//                 a = nums[i];
+//             }
+//         }
+//         return {a,b};
+//     }
+// };
 
 ///////t---o(n) + o(log n)
 ///////s---o(1)
 
 //////////////////
+
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        int n = nums.size() -2; ////given numbers = 0 .... n-1
+        int XOR = 0;  //a^b
+        for(int num : nums){
+            XOR ^= num;
+        }
+        for(int i=0;i<n;i++){
+            XOR ^= i;
+        }
+        int trailzerocount = __builtin_ctz(XOR);
+        int mask = (1 << trailzerocount);
+
+        int G1 = 0;
+        int G2 = 0;
+        for(int num : nums){
+            if(num & mask){
+                G1 ^= num;
+            }
+            else{
+                G2 ^= num;
+            }
+        }
+        for(int num =0;num<n;num++){
+            if(num & mask){
+                G1 ^= num;
+            }
+            else{
+                G2 ^= num;
+            }
+        }
+        return {G1 , G2};
+    }
+};
+
+
+
+
+
+//////////////////////////////
 
 // class Solution {
 // public:
