3560cfbef07320e268aa316223929c7c4cdbeda1
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-31 21:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (75.61%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,17 +1,17 @@
-// class Solution {
-// public:
-//     vector<int> getSneakyNumbers(vector<int>& nums) {
-//         sort(begin(nums) , end(nums));
-//         int a =0 ,b =0;
-//         for(int i=0;i<nums.size()-1;i++){
-//             if(nums[i] == nums[i+1]){
-//                 b = a;
-//                 a = nums[i];
-//             }
-//         }
-//         return {a,b};
-//     }
-// };
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        sort(begin(nums) , end(nums));
+        int a =0 ,b =0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]){
+                b = a;
+                a = nums[i];
+            }
+        }
+        return {a,b};
+    }
+};
 
 ///////t---o(n) + o(log n)
 ///////s---o(1)
@@ -34,19 +34,19 @@
 
 /////////
 
-class Solution {
-public:
-    vector<int> getSneakyNumbers(vector<int>& nums) {
-       unordered_map<int,int>mp;
-       for(int num : nums){
-            mp[num]++;
-       }
-       vector<int>ans;
-       for(auto &it : mp){
-        if(it.second == 2){
-            ans.push_back(it.first);
-        }
-       }
-         return ans;
-       }
-};
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//        unordered_map<int,int>mp;
+//        for(int num : nums){
+//             mp[num]++;
+//        }
+//        vector<int>ans;
+//        for(auto &it : mp){
+//         if(it.second == 2){
+//             ans.push_back(it.first);
+//         }
+//        }
+//          return ans;
+//        }
+// };
