a6c6ce8db1f3bcd55efc15d3060d0937ed610ede
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-10-31 21:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (75.61%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> getSneakyNumbers(vector<int>& nums) {
+//         sort(begin(nums) , end(nums));
+//         int a =0 ,b =0;
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i] == nums[i+1]){
+//                 b = a;
+//                 a = nums[i];
+//             }
+//         }
+//         return {a,b};
+//     }
+// };
+
+///////t---o(n)
+///////s---o(1)
+
+//////////////////
+
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         sort(begin(nums) , end(nums));
-        int a =0 ,b =0;
+        vector<int>ans;
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i] == nums[i+1]){
-                b = a;
-                a = nums[i];
+               ans.push_back(nums[i+1]);
             }
         }
-        return {a,b};
+        return ans;
     }
 };
