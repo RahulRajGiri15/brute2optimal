6e045ca3ec15f42397a4291b3e27a91e77f21d22
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-10 19:34:11 +0530

    Time: 47 ms (50.84%) | Memory: 222.2 MB (69.10%) - LeetSync

---
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums) {
+//         unordered_set<int> st(nums.begin(),nums.end());
+//         int n = nums.size();
+//         int ops = 0;
+
+     
+//         for(int target : st){
+//             if(target == 0){
+//                 continue;
+//             }
+//             bool flow = false;
+//             for(int i=0;i<n;i++){
+//                 if(target == nums[i]){
+//                     if(flow != true){
+//                         flow = true;
+//                         ops++;
+//                     }
+//                 }
+//                 else if(nums[i] < target){
+//                     flow = false;
+//                 }
+//             }
+//         }
+//         return ops;
+//     }
+// };
+
+
+//////////////////////////////
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+       int n = nums.size();
+       stack<int> st; ////use monotonic -increasing stack concept here 
+        int ops =0;
+       for(int i=0;i<n;i++){
+            while(!st.empty() && st.top() > nums[i]){
+                st.pop();
+            }
+            if(nums[i] == 0){
+                continue;
+            }
+           
+            if(st.empty() || st.top() < nums[i]){
+                st.push(nums[i]);
+                ops++;
+            }
+       }
+       return ops;
+    }
+};
