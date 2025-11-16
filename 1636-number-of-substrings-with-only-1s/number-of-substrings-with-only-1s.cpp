ee1965048e06d9768b1f99fbb4c367d95710aac3
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-16 18:16:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (39.33%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -21,26 +21,73 @@
 //     }
 // };
 
-////////////////////
+////////////////////using formula 
+/// (n * (n -1) / 2) -- n = no of 1 's in a substring 
+///if n = 2 -- than 2 * 3 /2 = 3 --- that means 3 substring of 1 is possible with 2 -- 1's
+
+// class Solution {
+// public:
+//     int numSub(string s) {
+//         int n = s.size();
+//         long long count = 0;
+//         const int M = 1e9 + 7;
+//         long long res = 0;
+//         for(int i=0;i<n;i++){ 
+//             if(s[i] == '1'){
+//                 count++;
+//             }
+//             else{
+//                 res += (count * (count +1)/2) % M;
+//                 count = 0;
+//             }
+//         }
+//         res += (count * (count + 1) /2) % M;
+
+//         return int(res);
+//     }
+// };
+
+/////////////////////most optimised approach ---- 
+
+// class Solution {
+// public:
+//     int numSub(string s) {
+//         int n = s.size();
+//         long long count = 0;
+//         const int M = 1e9 + 7;
+//         long long res = 0;
+//         for(int i=0;i<n;i++){ 
+//             if(s[i] == '1'){
+//                 count++;
+//                 res += count;
+//             }
+//             else{
+//                 count = 0;
+//             }
+//         }
+//         return int(res);
+//     }
+// };
+
+//////////////////////////
 
 class Solution {
 public:
     int numSub(string s) {
         int n = s.size();
-        long long count = 0;
         const int M = 1e9 + 7;
+
+        long long count = 0;
         long long res = 0;
         for(int i=0;i<n;i++){ 
             if(s[i] == '1'){
                 count++;
+                res = (res + count) % M;
             }
             else{
-                res += (count * (count +1)/2) % M;
                 count = 0;
             }
         }
-        res += (count * (count + 1) /2) % M;
-
         return int(res);
     }
 };
