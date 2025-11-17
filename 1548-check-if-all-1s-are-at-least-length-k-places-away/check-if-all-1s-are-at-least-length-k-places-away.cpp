1e50b6babded501150b68d5db5ac30d151760eb0
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-17 20:17:39 +0530

    Time: 4 ms (10.91%) | Memory: 61.3 MB (74.91%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,3 +1,31 @@
+// class Solution {
+// public:
+//     bool kLengthApart(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int count =0;
+//         int prev =0;
+//         int next = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i] == 1){
+//                 prev = i; /// now our prev is at first i
+//                 break;
+//             }
+//         }
+//         for(int i=prev +1;i<n;i++){
+//             if(nums[i] == 1){
+//                 next = i;
+//                 if((next-prev - 1) < k){
+//                     return false;
+//                 }
+//                 prev = next;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+///////////
+
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
@@ -5,13 +33,8 @@ public:
         int count =0;
         int prev =0;
         int next = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i] == 1){
-                prev = i; /// now our prev is at first i
-                break;
-            }
-        }
-        for(int i=prev +1;i<n;i++){
+        prev = -(k + 1); //// let last time seen 1 be at --> next-prev - 1 > k -- for 1 at idx 0
+        for(int i= 0;i<n;i++){
             if(nums[i] == 1){
                 next = i;
                 if((next-prev - 1) < k){
