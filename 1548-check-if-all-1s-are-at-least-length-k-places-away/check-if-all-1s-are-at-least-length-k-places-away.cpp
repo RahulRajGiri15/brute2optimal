64892cd352434d83f3cd341587dc77b87c24b6e1
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-17 20:08:11 +0530

    Time: 0 ms (100.00%) | Memory: 61.4 MB (47.64%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count =0;
+        int prev =0;
+        int next = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 1){
+                prev = i; /// now our prev is at first i
+                break;
+            }
+        }
+        for(int i=prev +1;i<n;i++){
+            if(nums[i] == 1){
+                next = i;
+                if((next-prev - 1) < k){
+                    return false;
+                }
+                prev = next;
+            }
+        }
+        return true;
+    }
+};
