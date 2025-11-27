63ce61b30dd7d929627a4ff170958782aa6403ba
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-27 22:16:34 +0530

    Time: 375 ms (80.72%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,23 @@
-# Write your MySQL query statement below
-select distinct author_id as id
+-- # Write your MySQL query statement below
+-- select distinct author_id as id
+-- from views
+-- -- where count(author_id = viewer_id) >= 1
+-- where author_id = viewer_id 
+-- order by id asc;
+
+--
+
+-- select author_id as id
+-- from views
+-- where author_id = viewer_id
+-- group by author_id
+-- having count(*) >= 1
+-- order by id asc
+
+-- 
+select author_id as id
 from views
--- where count(author_id = viewer_id) >= 1
-where author_id = viewer_id 
-order by id asc;
+where author_id = viewer_id
+group by author_id
+having count(id) >= 1
+order by id asc
