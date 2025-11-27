7f64f03d4bdfe551b2179db2779438a164433147
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-27 22:06:57 +0530

    Time: 374 ms (81.72%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-# DISTINCT -- here we use distinct to ensure each author is listed only once even if they have seen multiple of their own articles
-
-select DISTINCT author_id AS id 
-from Views 
+select distinct author_id as id
+from views
+-- where count(author_id = viewer_id) >= 1
 where author_id = viewer_id 
-order BY id ASC ;
+order by id asc;
