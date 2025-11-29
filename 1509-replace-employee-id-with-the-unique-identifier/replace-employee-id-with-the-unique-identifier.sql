c5e961ee79ed27fb6387840b8c54a27d8c2aa601
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-29 21:43:35 +0530

    Time: 1068 ms (72.81%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,23 +1,6 @@
 # Write your MySQL query statement below
 
-## solve is using LEFT JOIN
-
--- select EmployeeUNI.unique_id , Employees.name
--- FROM Employees 
--- LEFT JOIN EmployeeUNI 
--- ON Employees.id = EmployeeUNI.id;
-
-####OR
-
--- Select euni.unique_id , e.name 
--- from Employees as e
--- LEFT JOIN EmployeeUNI as euni
--- ON e.id = euni.id;
-
-
-####or
-
-Select euni.unique_id as unique_id , e.name as name  
-from Employees  e
-LEFT JOIN EmployeeUNI  euni
-ON e.id = euni.id;
+select eu.unique_id as unique_id , em.name as name 
+from Employees as em
+left Join EmployeeUNI as eu
+on em.id = eu.id
