ac90a621bef47275fb940978d265201417322c1d
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-11-30 23:24:45 +0530

    Time: 1227 ms (45.30%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 
-## here alos we will use left JOIN beacuse he have to show all data of sales table and only common data of Product table 
-
-select p.product_name , s.year, s.price 
+select p.product_name ,s.year ,s.price 
 from Sales s
-LEFT JOIN Product p
-ON s.product_id = p.product_id; 
+join Product p
+on p.product_id = s.product_id
