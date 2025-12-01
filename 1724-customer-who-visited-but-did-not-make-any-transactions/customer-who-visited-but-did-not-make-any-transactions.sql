6268747e8763251e7357ecf003cca0d8a8c19fc8
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-02 00:09:59 +0530

    Time: 1315 ms (41.52%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,35 +1,9 @@
 # Write your MySQL query statement below
-
-### 
-
-select vs.customer_id , COUNT(customer_id) as count_no_trans 
-from Visits vs
-LEFT JOIN Transactions t
-ON vs.visit_id = t.visit_id
-where t.transaction_id IS NULL
-GROUP BY vs.customer_id;
-
-
-### working of each line 
-
--- LEFT JOIN → keeps all rows from Visits even if there’s no match in Transactions.
-
--- ON vs.visit_id = t.visit_id → links visits to any transactions made during that   visit.
-
--- WHERE t.transaction_id IS NULL → filters only those visits where no transaction was made.
-
--- COUNT(customer_id) → counts how many such no-transaction visits each customer had.
-
--- GROUP BY vs.customer_id → groups by customer so we get one row per customer.
-
-
-
-
-#####OR
-
--- select vs.customer_id , COUNT(*) as count_no_trans 
--- from Visits vs
--- LEFT JOIN Transactions t
--- ON vs.visit_id = t.visit_id
--- where t.transaction_id IS NULL
--- GROUP BY vs.customer_id;
+select v.customer_id 
+, count(*) as count_no_trans
+from visits v
+left join Transactions t
+on v.visit_id = t.visit_id
+where transaction_id is null
+group by customer_id
+order by count_no_trans
