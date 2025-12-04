57b4b475bf9dc2515c2847138beaf04d7ea57140
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-04 23:53:15 +0530

    Time: 241 ms (44.44%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,28 +1,18 @@
 # Write your MySQL query statement below
 
-select a1.machine_id , ROUND(avg(a2.timestamp - a1.timestamp),3) as Processing_time
+-- select a.machine_id , round(avg(b.timestamp - a.timestamp),3) as processing_time
+-- -- select a.machine_id , b.timestamp , a.timestamp
+-- from Activity a
+-- join Activity b
+-- on a.machine_id = b.machine_id
+-- and a.process_id = b.process_id
+-- and a.timestamp < b.timestamp
+-- group by a.machine_id
+
+select a1.machine_id, round(avg(a2.timestamp - a1.timestamp),3) as Processing_time
 from Activity a1
-INNER JOIN Activity a2
-
-ON a1.process_id = a2.process_id 
-    AND a1.machine_id = a2.machine_id
-    # AND a2.timestamp > a1.timestamp
-    AND a2.activity_type = 'end'
-    AND a1.activity_type = 'start'
-
-GROUP BY a1.machine_id
-
-
-############OR
-
--- select a1.machine_id , ROUND(avg(a2.timestamp - a1.timestamp),3) as Processing_time
--- from Activity a1
--- INNER JOIN Activity a2
-
--- ON a1.process_id = a2.process_id 
---     AND a1.machine_id = a2.machine_id
---     AND a2.activity_type = 'end'
---     AND a1.activity_type = 'start'
---     AND a2.timestamp >= a1.timestamp
-      
--- GROUP BY a1.machine_id
+Join activity a2
+on a1.machine_id = a2.machine_id
+and a2.activity_type = 'end'
+and a1.activity_type = 'start'
+group by a1.machine_id
