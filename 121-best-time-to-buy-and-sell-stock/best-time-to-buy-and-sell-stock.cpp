5cde4c53e948d81ea86b8dabd2b5c78078dc2e29
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-06 18:47:18 +0530

    Time: 4 ms (18.66%) | Memory: 97.4 MB (25.52%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,40 +1,40 @@
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-//         int mini = prices[0];
-//         int maxprofit = 0;
-//         for(int i =0;i<prices.size();i++){
-//             if(mini > prices[i]){
-//                 mini = prices[i];
-//             }
-//             else{
-//                 maxprofit = max(maxprofit , prices[i] - mini);
-//             }
-//         }
-//         return maxprofit;
-//     }
-// };
-
-///////////////////////
-
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int mini = prices[0];
-        int maxi = prices[0];
         int maxprofit = 0;
-        int j = 0;
         for(int i =0;i<prices.size();i++){
             if(mini > prices[i]){
                 mini = prices[i];
-                maxi = mini; ///reset 
-                j = i;
             }
-            else if(prices[i] >maxi && i > j) {
-                maxi = prices[i];
+            else{
+                maxprofit = max(maxprofit , prices[i] - mini);
             }
-            maxprofit = max(maxprofit , maxi - mini);
         }
         return maxprofit;
     }
 };
+
+///////////////////////
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int mini = prices[0];
+//         int maxi = prices[0];
+//         int maxprofit = 0;
+//         int j = 0;
+//         for(int i =0;i<prices.size();i++){
+//             if(mini > prices[i]){
+//                 mini = prices[i];
+//                 maxi = mini; ///reset 
+//                 j = i;
+//             }
+//             else if(prices[i] >maxi && i > j) {
+//                 maxi = prices[i];
+//             }
+//             maxprofit = max(maxprofit , maxi - mini);
+//         }
+//         return maxprofit;
+//     }
+// };
