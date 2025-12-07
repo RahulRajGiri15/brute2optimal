6bec64b903365d29b29e0ad8e3a042b1ed6d1571
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-07 18:24:37 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (10.70%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,36 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_set<int>st(nums.begin(), nums.end());
+//         vector<int>res(begin(st),end(st));
+//         sort(begin(res),end(res));
+//         nums = res;
+//         return nums.size();
+//     }
+// };
+
+///////////////two pointer approach
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int>st(nums.begin(), nums.end());
-        vector<int>res(begin(st),end(st));
-        sort(begin(res),end(res));
-        nums = res;
+        int i =0 , j = 1;
+        vector<int>res;
+        res.push_back(nums[0]);
+        while(i<= j && j < n){
+
+           if(nums[i] == nums[j]){
+            j++;
+           }
+           else if(nums[i] != nums[j]){
+            i = j;
+            res.push_back(nums[i]);
+            j++;
+           }
+        }
+        nums= res;
         return nums.size();
     }
 };
