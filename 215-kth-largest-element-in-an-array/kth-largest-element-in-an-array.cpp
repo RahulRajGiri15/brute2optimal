3d04b00aa8ae89a73c4bd0cec72f23bdf031b59c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-08 18:56:34 +0530

    Time: 31 ms (59.72%) | Memory: 59.2 MB (84.52%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -18,17 +18,66 @@
 
 ////////////////////
 
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int>pq;
+//         for(int i=0;i<nums.size();i++){
+//             pq.push(nums[i]);
+//         }
+//         for(int i=0;i<k-1;i++){
+//             pq.pop();
+//         }
+//         int i = pq.top();
+//         return i;
+//     }
+// };
+
+
+////////////////////Hoore  partitaion ---> greater (left) < pivot < smaller (right)
+
 class Solution {
 public:
+    int partitation(int left, int right ,vector<int>& nums){
+        int pivot = nums[left];
+        int i = left+1;
+        int j = right;
+        while(i <= j){
+            if(nums[i] < pivot && nums[j] > pivot){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            if(nums[i] >= pivot){
+                i++;
+            }
+            if(nums[j] <= pivot){
+                j--;
+            }
+        }
+        swap(nums[left],nums[j]);
+        return j;
+    }
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int>pq;
-        for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]);
+        int n = nums.size();
+        int L = 0;
+        int R = n-1;
+        int pivot_idx = 0;
+
+        while(true){
+
+            pivot_idx = partitation(L,R, nums);
+            if(pivot_idx ==  k-1){
+                break;
+            }
+            else if(pivot_idx > k-1){
+                R = pivot_idx - 1;
+            }
+            else{
+                L = pivot_idx + 1;
+            }
         }
-        for(int i=0;i<k-1;i++){
-            pq.pop();
-        }
-        int i = pq.top();
-        return i;
+        return nums[pivot_idx];
     }
 };
