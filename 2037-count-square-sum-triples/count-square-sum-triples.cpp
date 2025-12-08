70ae9026be8b6d76282a203960ba3ba49df66dbc
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-08 20:06:32 +0530

    Time: 3 ms (90.49%) | Memory: 7.8 MB (55.75%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,13 +1,32 @@
+// class Solution {
+// public:
+//     int countTriples(int n) {
+//         int count = 0;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 for(int k=1;k<=n;k++){
+//                     if(pow(i,2) + pow(j,2) == pow(k,2)){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+/////////////
+
 class Solution {
 public:
     int countTriples(int n) {
         int count = 0;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if(pow(i,2) + pow(j,2) == pow(k,2)){
-                        count++;
-                    }
+            for(int j=i+1;j<=n;j++){
+                int sum = pow(i,2)+pow(j,2);
+                int x = sqrt(sum);
+                if(x * x == sum && x <= n){
+                    count += 2;
                 }
             }
         }
