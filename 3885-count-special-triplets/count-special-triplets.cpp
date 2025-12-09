887888da78770b23e56a8522e8ad01fd8e522982
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-10 03:16:31 +0530

    Time: 781 ms (63.31%) | Memory: 403 MB (56.12%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -18,24 +18,51 @@
 
 ///////////////////////
 
+// class Solution {
+// public:
+//     int specialTriplets(vector<int>& nums) {
+//         int M = 1e9 + 7;
+//         int n = nums.size();
+//         unordered_map<int,int>leftmp;
+//         unordered_map<int,int>rightmp;
+//         for(int i=0;i<n;i++){
+//             rightmp[nums[i]]++;
+//         }
+//         int count =0;
+//         for(int i=0;i<n;i++){
+//             rightmp[nums[i]]--;
+//             if(leftmp.contains(nums[i]*2) && rightmp.contains(nums[i]*2)){
+//                 count = (count + (1LL * leftmp[nums[i]*2] * rightmp[nums[i]*2])) % M;
+//             }
+//             leftmp[nums[i]]++;
+//         }
+//         return count;
+//     }
+// };
+
+//////////////////////
+
 class Solution {
 public:
+    int M = 1e9 + 7;
     int specialTriplets(vector<int>& nums) {
-        int M = 1e9 + 7;
+       
         int n = nums.size();
-        unordered_map<int,int>leftmp;
-        unordered_map<int,int>rightmp;
-        for(int i=0;i<n;i++){
-            rightmp[nums[i]]++;
-        }
-        int count =0;
-        for(int i=0;i<n;i++){
-            rightmp[nums[i]]--;
-            if(leftmp.contains(nums[i]*2) && rightmp.contains(nums[i]*2)){
-                count = (count + (1LL * leftmp[nums[i]*2] * rightmp[nums[i]*2])) % M;
+        unordered_map<int,int>valid_i;
+        unordered_map<int,int>valid_j;
+        int result = 0;
+        for (int &num : nums){
+            ///try to find valid_k
+            if(num % 2 == 0){
+                result = (result + valid_j[num/2]) % M;
             }
-            leftmp[nums[i]]++;
+            ///try finding valid j
+            valid_j[num] = (valid_j[num] + valid_i[num * 2]) % M;
+
+            valid_i[num]++;
         }
-        return count;
+
+        return result;
     }
 };
+
