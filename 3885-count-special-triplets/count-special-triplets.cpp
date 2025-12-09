273d52b4c8d86419db2bb7c4412539e7493a2ffc
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-10 02:38:15 +0530

    Time: 507 ms (86.33%) | Memory: 343.3 MB (78.78%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,25 +1,41 @@
+// class Solution {
+// public:
+//     int specialTriplets(vector<int>& nums) {
+//         int count = 0;
+//         int n = nums.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=i+1;j<n;j++){
+//                 for(int k = j+1;k<n;k++){
+//                     if(nums[i] == nums[j]*2 && nums[k] == nums[j]*2){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+///////////////////////
+
 class Solution {
 public:
-    int M = 1e9+7;
-
     int specialTriplets(vector<int>& nums) {
-        unordered_map<int, int> valid_i;
-        unordered_map<int, int> valid_j;
-
-        int result = 0;
-
-        for(int &num : nums) {
-            
-            if(num%2 == 0) { //if it's valid k, we got our triplet
-                result = (result + valid_j[num/2]) % M;
-            }
-
-            //If it's valid j or not
-            valid_j[num] = (valid_j[num] + valid_i[num*2]) % M;
-
-            valid_i[num]++;
+        int M = 1e9 + 7;
+        int n = nums.size();
+        unordered_map<int,int>leftmp;
+        unordered_map<int,int>rightmp;
+        for(int i=0;i<n;i++){
+            rightmp[nums[i]]++;
         }
-
-        return result;
+        int count =0;
+        for(int i=0;i<n;i++){
+            rightmp[nums[i]]--;
+            if(leftmp.contains(nums[i]*2) && rightmp.contains(nums[i]*2)){
+                count = (count + (1LL * leftmp[nums[i]*2] * rightmp[nums[i]*2])) % M;
+            }
+            leftmp[nums[i]]++;
+        }
+        return count;
     }
 };
