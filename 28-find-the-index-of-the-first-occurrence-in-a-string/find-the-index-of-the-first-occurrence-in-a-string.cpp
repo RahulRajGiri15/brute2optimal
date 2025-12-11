f18779dcf6ba79987d64da68b1f4bfaef764a66a
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2025-12-11 22:46:19 +0530

    Time: 131 ms (3.31%) | Memory: 9 MB (9.13%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,3 +1,58 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         if(needle.size() == 0 || haystack.size() == 0)return -1;
+//         int foundfrst = -1;
+//         for(int i=0;i<=haystack.size()-needle.size();i++){
+//             if(foundfrst != -1){
+//                 break;
+//             }
+//             int k =i;
+//             int j =0;
+//             bool mila = true;
+//             while(j<needle.size()){
+//                 if(haystack[i+j] != needle[j]){
+//                     mila = false;
+//                     break;
+//                 }
+//                 j++;
+//             }
+//             if(mila == true && foundfrst == -1){
+//                 foundfrst = k;
+//             }
+            
+//         }
+//         return foundfrst;
+//     }
+// };
+
+//////////////////////////////
+
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         if(needle.size() == 0 )return 0;
+//         if(haystack.size() == 0) return -1;
+
+//         for(int i=0;i<=haystack.size()-needle.size();i++){
+
+
+//             int j =0;
+           
+//             while(j< needle.size() && haystack[i+j] == needle[j]){
+//                 j++;
+//             }
+//             if(j == needle.size()) return i;
+
+            
+//         }
+//         return -1;
+//     }
+// };
+
+///////////
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
