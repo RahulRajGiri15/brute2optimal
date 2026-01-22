a758499cc8b0bb1539460bd851891a36fb62ee0e
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-01-22 09:05:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (58.75%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -1,11 +1,23 @@
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>ans(2*n,0);
+//         for(int i=0;i<n;i++){
+//             ans[i] = ans[i+n] = nums[i];
+//         }
+//         return ans;
+//     }
+// };
+
+/////////////////
+
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(2*n,0);
-        for(int i=0;i<n;i++){
-            ans[i] = ans[i+n] = nums[i];
-        }
+        vector<int>ans(nums.begin(),nums.end());
+        ans.insert(ans.end(),nums.begin(),nums.end()); /// insert(pos,begin,end)
         return ans;
     }
 };
