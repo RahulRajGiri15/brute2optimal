338d78583b7ea54c4b832d8d8b21177d202e63c1
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-02-11 13:21:59 +0530

    Time: 2348 ms (6.58%) | Memory: 20 MB (46.35%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,22 +1,35 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         ans = []
+#         n = len(nums)
+#         i = 0
+#         while(i < n):
+#             j = i+1
+#             while(j < n):
+#                 if(nums[i] + nums[j] == target):
+#                     ans.append(i)
+#                     ans.append(j)
+#                     return ans
+#                 j += 1
+
+#             i += 1
+
+#         return ans
+        
+    ########## 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         ans = []
         n = len(nums)
         i = 0
-        while(i < n-1):
+        while(i < n):
             j = i+1
             while(j < n):
                 if(nums[i] + nums[j] == target):
-                    ans.append(i)
-                    ans.append(j)
-                    return ans
+                    return [i , j]
                 j += 1
-
             i += 1
 
-        return ans
+        return []
         
-
-
-            
-
