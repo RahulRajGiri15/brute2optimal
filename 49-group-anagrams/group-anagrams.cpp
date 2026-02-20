cbac5912acb991606896cbce23e85e78e3d84f3c
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-02-20 12:20:39 +0530

    Time: 10 ms (92.87%) | Memory: 24.9 MB (78.38%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,59 +1,16 @@
-// class Solution {
-// public:
-//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-//         vector<vector<string>>ans;
-//         int n = strs.size();
-//         unordered_map<string,vector<string>>mp;
-        
-//         for(int i=0;i<n;i++){   ///////o(n)
-//             string st = strs[i];
-//             sort(st.begin(),st.end()); //// o(klogk)
-            
-//             mp[st].push_back(strs[i]);
-//         }
-//         for(auto &it : mp){
-//             ans.push_back(it.second);
-//         }
-//         return ans;
-//     }
-// };
-////t--o(n*klogk)
-////s--o(n*n)
-
-
-///////////////////////////////////////
-
 class Solution {
 public:
-    string generate(string word){
-        vector<int>alp(26,0);
-        for(char &c : word){
-            alp[c-'a']++;
-        }
-        string ans = "";
-        for(int i=0;i<26;i++){
-            int freq = alp[i];
-            if(freq > 0){
-                ans += string(freq,i+'a');
-            }
-        }
-        return ans;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>>ans;
-        int n = strs.size();
-        unordered_map<string,vector<string>>mp;
-        
-        for(int i=0;i<n;i++){   ///////o(n)
+        vector<vector<string>> result;
+        unordered_map<string, vector<string>>mp;
+        for(int i=0;i<strs.size();i++){
             string st = strs[i];
-
-            string newword = generate(st);
-            
-            mp[newword].push_back(strs[i]);
+            sort(begin(st) , end(st));
+            mp[st].push_back(strs[i]);
         }
         for(auto &it : mp){
-            ans.push_back(it.second);
+            result.push_back(it.second);
         }
-        return ans;
+        return result;
     }
 };
