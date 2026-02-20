b82a404a17a7e4202550381db4f09cd38fc90b82
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-02-20 14:54:26 +0530

    Time: 39 ms (7.23%) | Memory: 22 MB (61.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,38 @@
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res: List[List[str]] = []
+#         mp: dict[str, List[str]] = defaultdict(list)
+
+#         for i in range(0,len(strs),1):
+#             st = strs[i]
+#             key = ''.join(sorted(st))
+#             mp[key].append(strs[i])
+
+#         for key in mp:
+#             res.append(mp[key])
+#         return res
+
+
+# 
+
 class Solution:
+    def sortana(self , word: str) -> str:
+        freq : List[int] = [0]*26
+        res = ""
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+        for i in range(0,26,1):
+            if(freq[i] > 0):
+                res += chr(i+ ord('a')) * freq[i]
+        return res
+
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res: List[List[str]] = []
         mp: dict[str, List[str]] = defaultdict(list)
 
         for i in range(0,len(strs),1):
             st = strs[i]
-            key = ''.join(sorted(st))
+            key = self.sortana(st)
             mp[key].append(strs[i])
 
         for key in mp:
