691fbea28851f232ef63ec5eb0d23cbe033cbc85
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-02-20 13:03:58 +0530

    Time: 20 ms (39.19%) | Memory: 29.6 MB (20.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,12 +1,44 @@
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         vector<vector<string>> result;
+//         unordered_map<string, vector<string>>mp;
+//         for(int i=0;i<strs.size();i++){
+//             string st = strs[i];
+//             sort(begin(st) , end(st));
+//             mp[st].push_back(strs[i]);
+//         }
+//         for(auto &it : mp){
+//             result.push_back(it.second);
+//         }
+//         return result;
+//     }
+// };
+
+// 
+
 class Solution {
 public:
+    string makesort(string word){
+        string ans = "";
+        vector<int>freqw(26,0);
+        for(char &ch : word){
+            freqw[ch -'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freqw[i] > 0){
+                ans +=string(freqw[i] , i + 'a');
+            }
+        }
+        return ans;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result;
         unordered_map<string, vector<string>>mp;
         for(int i=0;i<strs.size();i++){
             string st = strs[i];
-            sort(begin(st) , end(st));
-            mp[st].push_back(strs[i]);
+            string newword = makesort(st);
+            mp[newword].push_back(strs[i]);
         }
         for(auto &it : mp){
             result.push_back(it.second);
@@ -14,3 +46,4 @@ public:
         return result;
     }
 };
+
