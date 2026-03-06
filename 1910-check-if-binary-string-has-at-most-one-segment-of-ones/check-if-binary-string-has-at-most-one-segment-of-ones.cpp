949a62acce82fcfaf39f15b82750e1faec3c97d8
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-03-07 01:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.67%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -17,15 +17,26 @@
 
 //////////////////
 
+// class Solution {
+// public:
+//     bool checkOnesSegment(string s) {
+//         int count = 1;
+//         for(int i = 0; i<s.size() -1; i++){
+//             if(s[i] == '0' && s[i+1] == '1'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+///////////////////
+
+
+
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int count = 1;
-        for(int i = 0; i<s.size() -1; i++){
-            if(s[i] == '0' && s[i+1] == '1'){
-                return false;
-            }
-        }
-        return true;
+        return s.find("01") == string::npos;
     }
 };
