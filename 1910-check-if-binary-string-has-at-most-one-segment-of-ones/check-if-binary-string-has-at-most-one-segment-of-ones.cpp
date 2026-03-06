aeb97900ac4df3547ba11c10b8ef9640b773f42e
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-03-07 01:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.67%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,15 +1,31 @@
+// class Solution {
+// public:
+//     bool checkOnesSegment(string s) {
+//         int count = 1;
+//         for(int i = 0; i<s.size() -1; i++){
+//             if( s[i] == '0' && s[i+1] == '1'){
+//                 count++;
+//             }
+//         }
+//         if(count == 1){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+//////////////////
+
 class Solution {
 public:
     bool checkOnesSegment(string s) {
         int count = 1;
         for(int i = 0; i<s.size() -1; i++){
-            if( s[i] == '0' && s[i+1] == '1'){
-                count++;
+            if(s[i] == '0' && s[i+1] == '1'){
+                return false;
             }
         }
-        if(count == 1){
-            return true;
-        }
-        return false;
+        return true;
     }
 };
