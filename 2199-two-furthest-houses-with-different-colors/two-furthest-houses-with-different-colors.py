c280d91cb8fd7ad1536426997bd70d7f53c6b3c3
Rahul Kumar Giri <160770727+RahulRajGiri15@users.noreply.github.com>
2026-04-20 20:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.41%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
@@ -10,12 +10,24 @@
         
 #         return maxd
 
+# class Solution:
+#     def maxDistance(self, colors: List[int]) -> int:
+#         maxd = float('-inf')
+#         for i in range(0,len(colors)):
+#             for j in range(i+1,len(colors)):
+#                 if(colors[i] != colors[j]):
+#                     maxd = max(abs(j-i), maxd)
+        
+#         return maxd
+
+
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
         maxd = float('-inf')
-        for i in range(0,len(colors)):
-            for j in range(i+1,len(colors)):
-                if(colors[i] != colors[j]):
-                    maxd = max(abs(j-i), maxd)
-        
+        n = len(colors)
+        for i in range(n-1,0,-1):
+            if(colors[i] != colors[0]):
+                maxd = max(maxd, abs(i-0))
+                maxd = max(maxd , abs((n-1)-i))
+
         return maxd
